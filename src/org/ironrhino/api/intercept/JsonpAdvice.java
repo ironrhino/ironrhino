8b23d593c708c9e693bc4ded34b295c88d26ae91
zhouyanming <zhouyanming@gmail.com>
2014-09-05 09:37:54 +0800

    x

---
+++ src/org/ironrhino/api/intercept/JsonpAdvice.java
@@ -0,0 +1,12 @@
+package org.ironrhino.api.intercept;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+@ControllerAdvice
+public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+
+	public JsonpAdvice() {
+		super("callback");
+	}
+}
