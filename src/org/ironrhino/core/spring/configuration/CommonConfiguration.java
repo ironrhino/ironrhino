b07eadb8301909cf55d8580185e009cd3912d6b0
zhouyanming <zhouyanming@gmail.com>
2014-12-26 08:58:14 +0800

    remove useless code

--- src/org/ironrhino/core/spring/configuration/CommonConfiguration.java
+++ src/org/ironrhino/core/spring/configuration/CommonConfiguration.java
@@ -2,14 +2,10 @@ package org.ironrhino.core.spring.configuration;
 
 import java.util.Collections;
 
-import org.ironrhino.core.servlet.HttpErrorHandler;
 import org.ironrhino.core.spring.converter.DateConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.ConversionServiceFactoryBean;
@@ -21,7 +17,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 		@PropertySource(ignoreResourceNotFound = true, value = "classpath:resources/spring/applicationContext.${STAGE}.properties"),
 		@PropertySource(ignoreResourceNotFound = true, value = "file:${app.home}/conf/applicationContext.properties"),
 		@PropertySource(ignoreResourceNotFound = true, value = "file:${app.home}/conf/applicationContext.${STAGE}.properties") })
-@ComponentScan(excludeFilters = @Filter(value = HttpErrorHandler.class, type = FilterType.ASSIGNABLE_TYPE))
 @EnableAspectJAutoProxy
 public class CommonConfiguration {
 
