2d2a160a6279e33a2e4ee8c88d7fb64fb6a6775f
zhouyanming <zhouyanming@gmail.com>
2015-01-16 12:32:14 +0800

    x

--- src/org/ironrhino/rest/RestFilter.java
+++ src/org/ironrhino/rest/RestFilter.java
@@ -14,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.ironrhino.core.util.RequestUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class RestFilter extends OncePerRequestFilter {
 
 	public static final String PARAMETER_NAME_METHOD = "_method";
-	public static final String HEADER_NAME_CONTENT_TYPE = "Content-Type";
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
@@ -30,8 +30,8 @@ public class RestFilter extends OncePerRequestFilter {
 				.parseParametersFromQueryString(request.getQueryString());
 		String method = map.get(PARAMETER_NAME_METHOD);
 		if (StringUtils.isNotBlank(method)
-				|| request.getContentType() == null
-				|| !request.getContentType().startsWith(
+				|| request.getContentType() != null
+				&& !request.getContentType().startsWith(
 						MediaType.APPLICATION_JSON_VALUE))
 			request = new WrappedHttpServletRequest(request,
 					StringUtils.isNotBlank(method) ? method.toUpperCase()
@@ -62,7 +62,7 @@ public class RestFilter extends OncePerRequestFilter {
 
 		@Override
 		public String getHeader(String name) {
-			if (StringUtils.equalsIgnoreCase(HEADER_NAME_CONTENT_TYPE, name)) {
+			if (StringUtils.equalsIgnoreCase(HttpHeaders.CONTENT_TYPE, name)) {
 				return MediaType.APPLICATION_JSON_VALUE;
 			}
 			return super.getHeader(name);
@@ -70,7 +70,7 @@ public class RestFilter extends OncePerRequestFilter {
 
 		@Override
 		public Enumeration<String> getHeaders(String name) {
-			if (StringUtils.equalsIgnoreCase(HEADER_NAME_CONTENT_TYPE, name)) {
+			if (StringUtils.equalsIgnoreCase(HttpHeaders.CONTENT_TYPE, name)) {
 				return Collections.enumeration(Arrays
 						.asList(MediaType.APPLICATION_JSON_VALUE));
 			}
--- src/org/ironrhino/sample/api/AppInitializer.java
+++ src/org/ironrhino/sample/api/AppInitializer.java
@@ -1,9 +1,14 @@
 package org.ironrhino.sample.api;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.ironrhino.rest.RestFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -13,6 +18,7 @@ public class AppInitializer implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext)
 			throws ServletException {
+
 		ServletRegistration.Dynamic dynamic = servletContext.addServlet("api",
 				DispatcherServlet.class);
 		dynamic.setInitParameter("contextClass",
@@ -22,6 +28,9 @@ public class AppInitializer implements WebApplicationInitializer {
 		dynamic.addMapping("/api/*");
 		dynamic.setAsyncSupported(true);
 		dynamic.setLoadOnStartup(1);
+		FilterRegistration.Dynamic dynamicFilter = servletContext.addFilter(
+				"restFilter", RestFilter.class);
+		dynamicFilter.addMappingForServletNames(
+				EnumSet.of(DispatcherType.REQUEST), false, "api");
 	}
-
 }
