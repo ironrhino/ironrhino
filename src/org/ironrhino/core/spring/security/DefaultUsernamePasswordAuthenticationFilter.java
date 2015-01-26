9635993f269bd540ee5cae0957b69132090a8602
zhouyanming <zhouyanming@gmail.com>
2015-01-26 16:38:35 +0800

    replace 'endsWith' with 'equals' when match uri

--- src/org/ironrhino/core/spring/security/DefaultUsernamePasswordAuthenticationFilter.java
+++ src/org/ironrhino/core/spring/security/DefaultUsernamePasswordAuthenticationFilter.java
@@ -13,9 +13,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.UrlUtils;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.Assert;
 
 public class DefaultUsernamePasswordAuthenticationFilter extends
 		UsernamePasswordAuthenticationFilter {
@@ -28,22 +25,10 @@ public class DefaultUsernamePasswordAuthenticationFilter extends
 
 	public static final String TARGET_URL = "targetUrl";
 
-	private String filterProcessesUrl;
-
-	public String getFilterProcessesUrl() {
-		return filterProcessesUrl;
-	}
-
-	public void setFilterProcessesUrl(String filterProcessesUrl) {
-		this.filterProcessesUrl = filterProcessesUrl;
-	}
-
 	@PostConstruct
 	public void init() {
 		setAuthenticationFailureHandler(authenticationFailureHandler);
 		setAuthenticationSuccessHandler(authenticationSuccessHandler);
-		setRequiresAuthenticationRequestMatcher(new FilterProcessUrlRequestMatcher(
-				getFilterProcessesUrl()));
 	}
 
 	public void success(HttpServletRequest request,
@@ -56,36 +41,6 @@ public class DefaultUsernamePasswordAuthenticationFilter extends
 			HttpServletResponse response, AuthenticationException failed)
 			throws IOException, ServletException {
 		super.unsuccessfulAuthentication(request, response, failed);
-	}
-
-	// replace "endsWith" with "equals"
-	private static final class FilterProcessUrlRequestMatcher implements
-			RequestMatcher {
-		private final String filterProcessesUrl;
-
-		private FilterProcessUrlRequestMatcher(String filterProcessesUrl) {
-			Assert.hasLength(filterProcessesUrl,
-					"filterProcessesUrl must be specified");
-			Assert.isTrue(UrlUtils.isValidRedirectUrl(filterProcessesUrl),
-					filterProcessesUrl + " isn't a valid redirect URL");
-			this.filterProcessesUrl = filterProcessesUrl;
-		}
-
-		public boolean matches(HttpServletRequest request) {
-			String uri = request.getRequestURI();
-			int pathParamIndex = uri.indexOf(';');
-
-			if (pathParamIndex > 0) {
-				// strip everything after the first semi-colon
-				uri = uri.substring(0, pathParamIndex);
-			}
-
-			if ("".equals(request.getContextPath())) {
-				return uri.equals(filterProcessesUrl);
-			}
-
-			return uri.equals(request.getContextPath() + filterProcessesUrl);
-		}
 	}
 
 }
