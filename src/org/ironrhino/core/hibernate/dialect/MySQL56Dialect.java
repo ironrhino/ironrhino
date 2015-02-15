0b0c4af08399415fa21c2c44d6b99e50a1ace99e
zhouyanming <zhouyanming@gmail.com>
2015-02-15 08:45:04 +0800

    add timestamp functions

--- src/org/ironrhino/core/hibernate/dialect/MySQL56Dialect.java
+++ src/org/ironrhino/core/hibernate/dialect/MySQL56Dialect.java
@@ -3,10 +3,22 @@ package org.ironrhino.core.hibernate.dialect;
 import java.sql.Types;
 
 import org.hibernate.dialect.MySQL5Dialect;
+import org.hibernate.dialect.function.SQLFunction;
+import org.hibernate.dialect.function.StaticPrecisionFspTimestampFunction;
 
 public class MySQL56Dialect extends MySQL5Dialect {
 	public MySQL56Dialect() {
 		super();
 		registerColumnType(Types.TIMESTAMP, "datetime(6)");
+
+		// cp from MySQL57InnoDBDialect
+		final SQLFunction currentTimestampFunction = new StaticPrecisionFspTimestampFunction(
+				"now", 6);
+		registerFunction("now", currentTimestampFunction);
+		registerFunction("current_timestamp", currentTimestampFunction);
+		registerFunction("localtime", currentTimestampFunction);
+		registerFunction("localtimestamp", currentTimestampFunction);
+		registerFunction("sysdate", new StaticPrecisionFspTimestampFunction(
+				"sysdate", 6));
 	}
 }
