0128735b94bb232a91f75fba310a435e9048470f
zhouyanming <zhouyanming@gmail.com>
2015-05-11 15:31:31 +0800

    upgrade tomcat and redis

--- docs/install.ubuntu.sh
+++ docs/install.ubuntu.sh
@@ -46,7 +46,7 @@ fi
 #install tomcat
 if [ ! -d tomcat8080 ];then
 if ! $(ls -l apache-tomcat-*.tar.gz >/dev/null 2>&1) ; then
-wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
+wget http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.61/bin/apache-tomcat-7.0.61.tar.gz
 fi
 tar xf apache-tomcat-*.tar.gz >/dev/null && rm -rf apache-tomcat-*.tar.gz
 rename s/^apache-tomcat.*$/tomcat/g apache-tomcat-*
@@ -485,7 +485,7 @@ fi
 
 #install redis
 if ! which redis-server > /dev/null && ! $(ls -l redis-*.tar.gz >/dev/null 2>&1) ; then
-wget http://download.redis.io/releases/redis-2.8.13.tar.gz
+wget http://download.redis.io/releases/redis-3.0.1.tar.gz
 fi
 if $(ls -l redis-*.tar.gz >/dev/null 2>&1) ; then
 tar xf redis-*.tar.gz >/dev/null && rm -rf redis-*.tar.gz
