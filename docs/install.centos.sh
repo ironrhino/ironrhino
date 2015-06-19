7efb7fe8c84c0dffd98a4798f5f7c577e5a84044
zhouyanming <zhouyanming@gmail.com>
2015-06-19 08:45:24 +0800

    nginx configuration proxy_set_header  X-Url-Scheme $scheme;

--- docs/install.centos.sh
+++ docs/install.centos.sh
@@ -283,6 +283,7 @@ server {
                  proxy_http_version 1.1;
                  proxy_set_header  X-Forwarded-For  \$proxy_add_x_forwarded_for;
                  proxy_set_header  X-Real-IP  \$remote_addr;
+                 proxy_set_header  X-Url-Scheme \$scheme;
                  proxy_set_header  Host \$http_host;
                  proxy_set_header Upgrade \$http_upgrade;
                  proxy_set_header Connection "upgrade";
@@ -293,6 +294,7 @@ server {
                  proxy_redirect    off;
                  proxy_set_header  X-Forwarded-For  \$proxy_add_x_forwarded_for;
                  proxy_set_header  X-Real-IP  \$remote_addr;
+                 proxy_set_header  X-Url-Scheme \$scheme;
                  proxy_set_header  Host \$http_host;
                  limit_conn addr   8;
         }
