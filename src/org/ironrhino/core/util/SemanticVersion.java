aa3766967ca90ff75130fcb32c65eee88ab86dab
zhouyanming <zhouyanming@gmail.com>
2015-07-17 10:24:02 +0800

    add prerelease and build

--- src/org/ironrhino/core/util/SemanticVersion.java
+++ src/org/ironrhino/core/util/SemanticVersion.java
@@ -9,12 +9,24 @@ public class SemanticVersion implements Serializable, Comparable<SemanticVersion
 	private int major;
 	private int minor;
 	private Integer patch;
+	private String prerelease;
+	private String build;
 
 	public SemanticVersion() {
 
 	}
 
 	public SemanticVersion(String semver) {
+		int i = semver.indexOf('+');
+		if (i > 0) {
+			build = semver.substring(i + 1);
+			semver = semver.substring(0, i);
+		}
+		i = semver.indexOf('-');
+		if (i > 0) {
+			prerelease = semver.substring(i + 1);
+			semver = semver.substring(0, i);
+		}
 		String[] arr = semver.split("\\.");
 		major = Integer.parseInt(arr[0]);
 		if (arr.length > 1)
@@ -47,6 +59,22 @@ public class SemanticVersion implements Serializable, Comparable<SemanticVersion
 		this.patch = patch;
 	}
 
+	public String getPrerelease() {
+		return prerelease;
+	}
+
+	public void setPrerelease(String prerelease) {
+		this.prerelease = prerelease;
+	}
+
+	public String getBuild() {
+		return build;
+	}
+
+	public void setBuild(String build) {
+		this.build = build;
+	}
+
 	@Override
 	public int compareTo(SemanticVersion that) {
 		if (that == null)
@@ -76,6 +104,10 @@ public class SemanticVersion implements Serializable, Comparable<SemanticVersion
 		sb.append(major).append(".").append(minor);
 		if (patch != null)
 			sb.append(".").append(patch);
+		if (org.apache.commons.lang3.StringUtils.isNotBlank(prerelease))
+			sb.append("-").append(prerelease);
+		if (org.apache.commons.lang3.StringUtils.isNotBlank(build))
+			sb.append("+").append(build);
 		return sb.toString();
 	}
 
