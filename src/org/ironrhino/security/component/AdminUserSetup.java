d7c0bd71da7ee434273d142746c0623274e5a988
Yanming Zhou <zhouyanming@gmail.com>
2015-08-17 10:31:56 +0800

    polishing

--- src/org/ironrhino/security/component/AdminUserSetup.java
+++ src/org/ironrhino/security/component/AdminUserSetup.java
@@ -3,7 +3,6 @@ package org.ironrhino.security.component;
 import org.ironrhino.core.metadata.Setup;
 import org.ironrhino.core.metadata.SetupParameter;
 import org.ironrhino.core.security.role.UserRole;
-import org.ironrhino.core.util.CodecUtils;
 import org.ironrhino.security.model.User;
 import org.ironrhino.security.service.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +26,7 @@ public class AdminUserSetup {
 			return null;
 		User admin = new User();
 		admin.setUsername(username);
-		admin.setPassword(CodecUtils.digest(password));
+		admin.setLegiblePassword(password);
 		admin.setEnabled(true);
 		admin.getRoles().add(UserRole.ROLE_ADMINISTRATOR);
 		userManager.save(admin);
