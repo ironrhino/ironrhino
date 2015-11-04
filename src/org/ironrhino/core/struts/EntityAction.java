05bbe24f98b738f7f969408ea686f485e7b2663c
Yanming Zhou <zhouyanming@gmail.com>
2015-11-04 12:55:12 +0800

    Add hook beforeSave and afterSave

--- src/org/ironrhino/core/struts/EntityAction.java
+++ src/org/ironrhino/core/struts/EntityAction.java
@@ -730,7 +730,9 @@ public class EntityAction<EN extends Persistable<?>> extends BaseAction {
 			}
 		}
 		BaseManager<Persistable<?>> entityManager = getEntityManager(getEntityClass());
+		beforeSave((EN) _entity);
 		entityManager.save(_entity);
+		afterSave((EN) _entity);
 		addActionMessage(getText("save.success"));
 		return SUCCESS;
 	}
@@ -1059,6 +1061,14 @@ public class EntityAction<EN extends Persistable<?>> extends BaseAction {
 			logger.error(e.getMessage(), e);
 		}
 		return true;
+	}
+
+	protected void beforeSave(EN en) {
+
+	}
+
+	protected void afterSave(EN en) {
+
 	}
 
 	private boolean checkEntityReadonly(String expression, Persistable<?> entity) {
