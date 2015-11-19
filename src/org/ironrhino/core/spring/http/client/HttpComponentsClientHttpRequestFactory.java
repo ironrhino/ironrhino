4380c5ecee2a776eca60ba2c297699a08af5ae0a
Yanming Zhou <zhouyanming@gmail.com>
2015-11-19 11:47:55 +0800

    Fix trustAllHosts

--- src/org/ironrhino/core/spring/http/client/HttpComponentsClientHttpRequestFactory.java
+++ src/org/ironrhino/core/spring/http/client/HttpComponentsClientHttpRequestFactory.java
@@ -1,9 +1,19 @@
 package org.ironrhino.core.spring.http.client;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 import org.apache.http.NoHttpResponseException;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.ironrhino.core.servlet.AccessFilter;
 import org.slf4j.MDC;
 
@@ -16,8 +26,20 @@ public class HttpComponentsClientHttpRequestFactory
 
 	public HttpComponentsClientHttpRequestFactory(boolean trustAllHosts) {
 		HttpClientBuilder builder = builder();
-		if (trustAllHosts)
-			builder.setSSLHostnameVerifier((name, session) -> true);
+		if (trustAllHosts) {
+			try {
+				SSLContextBuilder sbuilder = SSLContexts.custom().loadTrustMaterial(null, new TrustStrategy() {
+					@Override
+					public boolean isTrusted(final X509Certificate[] chain, final String authType)
+							throws CertificateException {
+						return true;
+					}
+				});
+				builder.setSSLSocketFactory(new SSLConnectionSocketFactory(sbuilder.build()));
+			} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+				e.printStackTrace();
+			}
+		}
 		setHttpClient(builder.build());
 	}
 
--- src/org/ironrhino/core/spring/http/client/SimpleClientHttpRequestFactory.java
+++ src/org/ironrhino/core/spring/http/client/SimpleClientHttpRequestFactory.java
@@ -2,8 +2,15 @@ package org.ironrhino.core.spring.http.client;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.ironrhino.core.servlet.AccessFilter;
 import org.slf4j.MDC;
@@ -26,7 +33,25 @@ public class SimpleClientHttpRequestFactory extends org.springframework.http.cli
 		super.prepareConnection(connection, httpMethod);
 		if (trustAllHosts && connection instanceof HttpsURLConnection) {
 			HttpsURLConnection https = (HttpsURLConnection) connection;
-			https.setHostnameVerifier((name, session) -> true);
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				public void checkClientTrusted(X509Certificate[] certs, String authType) {
+				}
+
+				public void checkServerTrusted(X509Certificate[] certs, String authType) {
+				}
+			} };
+			try {
+				SSLContext sc = SSLContext.getInstance("TLS");
+				sc.init(null, trustAllCerts, new java.security.SecureRandom());
+				SSLSocketFactory ssf = sc.getSocketFactory();
+				https.setSSLSocketFactory(ssf);
+			} catch (KeyManagementException | NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
 		}
 		String requestId = MDC.get(AccessFilter.MDC_KEY_REQUEST_ID);
 		if (requestId != null)
