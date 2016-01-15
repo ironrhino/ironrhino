41ee724f69406d884489652e62daf2920851d7f9
Yanming Zhou <zhouyanming@gmail.com>
2016-01-15 10:09:46 +0800

    Refactor RedisQueue

--- src/org/ironrhino/core/redis/RedisQueue.java
+++ src/org/ironrhino/core/redis/RedisQueue.java
@@ -2,20 +2,37 @@ package org.ironrhino.core.redis;
 
 import java.io.Serializable;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import org.ironrhino.core.util.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.support.collections.DefaultRedisList;
 
 public abstract class RedisQueue<T extends Serializable> implements org.ironrhino.core.message.Queue<T> {
 
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
 	protected String queueName;
+
+	protected boolean consuming;
+
+	private boolean stopConsuming;
+
+	@Autowired(required = false)
+	private ExecutorService executorService;
 
 	@Autowired
 	private RedisTemplate<String, T> redisTemplate;
+
+	public void setConsuming(boolean consuming) {
+		this.consuming = consuming;
+	}
 
 	public void setQueueName(String queueName) {
 		this.queueName = queueName;
@@ -33,9 +50,29 @@ public abstract class RedisQueue<T extends Serializable> implements org.ironrhin
 	}
 
 	@PostConstruct
-	public void afterPropertiesSet() {
+	public void init() {
 		queue = new DefaultRedisList<>(queueName, redisTemplate);
+		if (consuming) {
+			Runnable task = () -> {
+				while (!stopConsuming) {
+					try {
+						T message = queue.take();
+						consume(message);
+					} catch (Throwable e) {
+						logger.error(e.getMessage(), e);
+					}
+				}
+			};
+			if (executorService != null)
+				executorService.execute(task);
+			else
+				new Thread(task).start();
+		}
+	}
 
+	@PreDestroy
+	public void stop() {
+		stopConsuming = true;
 	}
 
 	@Override
