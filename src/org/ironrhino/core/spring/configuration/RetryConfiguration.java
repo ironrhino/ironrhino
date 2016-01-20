03c445066789c29aa61f99d50194681d2445c4d0
Yanming Zhou <zhouyanming@gmail.com>
2016-01-20 15:21:14 +0800

    Conditional register RetryConfiguration

--- src/org/ironrhino/core/spring/configuration/RetryConfiguration.java
+++ src/org/ironrhino/core/spring/configuration/RetryConfiguration.java
@@ -1,14 +1,44 @@
 package org.ironrhino.core.spring.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ClassUtils;
 
-@SuppressWarnings("serial")
 @Configuration
-public class RetryConfiguration extends org.springframework.retry.annotation.RetryConfiguration {
+public class RetryConfiguration implements BeanDefinitionRegistryPostProcessor {
+
+	private static final String CLASS_NAME = "org.springframework.retry.annotation.RetryConfiguration";
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public int getOrder() {
-		return -100;
+	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+		if (!ClassUtils.isPresent(CLASS_NAME, getClass().getClassLoader())) {
+			logger.warn("undetected class {}", CLASS_NAME);
+			return;
+		}
+		try {
+			Class<?> clazz = Class.forName(CLASS_NAME);
+			RootBeanDefinition beanDefinition = new RootBeanDefinition(clazz);
+			MutablePropertyValues propertyValues = new MutablePropertyValues();
+			propertyValues.addPropertyValue("order", "-100");
+			beanDefinition.setPropertyValues(propertyValues);
+			registry.registerBeanDefinition(CLASS_NAME, beanDefinition);
+		} catch (ClassNotFoundException e) {
+			logger.error(e.getMessage(), e);
+		}
+
 	}
 
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+	}
 }
