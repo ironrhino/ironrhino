da570bf8443b99033ea94a9498512a53c34e1cad
Yanming Zhou <zhouyanming@gmail.com>
2016-01-20 11:06:23 +0800

    Use fullname as beanName for @Configuration beans

--- src/org/ironrhino/core/spring/NameGenerator.java
+++ src/org/ironrhino/core/spring/NameGenerator.java
@@ -4,6 +4,7 @@ import java.beans.Introspector;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ClassUtils;
 
 public class NameGenerator extends AnnotationBeanNameGenerator {
@@ -14,6 +15,16 @@ public class NameGenerator extends AnnotationBeanNameGenerator {
 	}
 
 	public static String buildDefaultBeanName(String beanClassName) {
+		if (beanClassName.startsWith("org.ironrhino") && beanClassName.endsWith("Configuration")) {
+			try {
+				Class<?> clazz = Class.forName(beanClassName);
+				if (clazz.isAnnotationPresent(Configuration.class))
+					return beanClassName;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		}
 		String shortClassName = ClassUtils.getShortName(beanClassName);
 		if (shortClassName.endsWith("Impl") && shortClassName.length() > 4)
 			shortClassName = shortClassName.substring(0, shortClassName.length() - 4);
