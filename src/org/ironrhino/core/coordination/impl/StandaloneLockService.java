7c3d2534b675acfeab108ab3892e8cc3f09d21f7
Yanming Zhou <zhouyanming@gmail.com>
2016-03-06 11:57:49 +0800

    Revert to use ConcurrentHashMap

--- src/org/ironrhino/core/coordination/impl/StandaloneLockService.java
+++ src/org/ironrhino/core/coordination/impl/StandaloneLockService.java
@@ -2,33 +2,20 @@ package org.ironrhino.core.coordination.impl;
 
 import static org.ironrhino.core.metadata.Profiles.DEFAULT;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
-
-import javax.annotation.PostConstruct;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.ironrhino.core.coordination.LockService;
 import org.ironrhino.core.spring.configuration.ServiceImplementationConditional;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.google.common.util.concurrent.Striped;
 
 @Component("lockService")
 @ServiceImplementationConditional(profiles = DEFAULT)
 public class StandaloneLockService implements LockService {
 
-	@Value("${lockService.lockStripes:0}")
-	private int lockStripes = 0;
-
-	private Striped<Lock> stripedLocks;
-
-	@PostConstruct
-	public void init() {
-		if (lockStripes <= 0)
-			lockStripes = Runtime.getRuntime().availableProcessors() * 4;
-		stripedLocks = Striped.lazyWeakLock(lockStripes);
-	}
+	private ConcurrentHashMap<String, Lock> locks = new ConcurrentHashMap<>();
 
 	@Override
 	public boolean tryLock(String name) {
@@ -52,10 +39,21 @@ public class StandaloneLockService implements LockService {
 
 	@Override
 	public void unlock(String name) {
-		getLock(name).unlock();
+		Lock lock = locks.get(name);
+		if (lock == null)
+			throw new IllegalArgumentException("Lock " + name + " is not held");
+		lock.unlock();
 	}
 
 	private Lock getLock(String name) {
-		return stripedLocks.get(name);
+		Lock lock = locks.get(name);
+		if (lock == null) {
+			Lock newLock = new ReentrantLock();
+			lock = locks.putIfAbsent(name, newLock);
+			if (lock == null)
+				lock = newLock;
+		}
+		return lock;
 	}
+
 }
