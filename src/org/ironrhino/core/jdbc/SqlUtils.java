672cf2cc027e6ec1a565a06b6f5cc3383a7f1f84
Yanming Zhou <zhouyanming@gmail.com>
2016-03-09 17:18:54 +0800

    Add buildInsertStatement and buildUpdateStatement

--- src/org/ironrhino/core/jdbc/SqlUtils.java
+++ src/org/ironrhino/core/jdbc/SqlUtils.java
@@ -1,14 +1,77 @@
 package org.ironrhino.core.jdbc;
 
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanWrapperImpl;
 
 public class SqlUtils {
+
+	public static String buildInsertStatement(Class<?> beanClass) {
+		return buildInsertStatement(beanClass, null);
+	}
+
+	public static String buildInsertStatement(Class<?> beanClass, String tableName, String... ignoreProperties) {
+		List<String> properties = new ArrayList<>();
+		Set<String> set = new HashSet<>();
+		if (ignoreProperties.length > 0)
+			set.addAll(Arrays.asList(ignoreProperties));
+		BeanWrapperImpl bw = new BeanWrapperImpl(beanClass);
+		for (PropertyDescriptor pd : bw.getPropertyDescriptors()) {
+			if (pd.getReadMethod() != null && pd.getWriteMethod() != null) {
+				String name = pd.getName();
+				if (!set.contains(name))
+					properties.add(name);
+			}
+		}
+		if (properties.isEmpty())
+			throw new IllegalArgumentException(beanClass + " has no properties");
+		if (tableName == null)
+			tableName = beanClass.getSimpleName();
+		StringBuilder sb = new StringBuilder("insert into ");
+		sb.append(tableName);
+		sb.append("(");
+		sb.append(StringUtils.join(properties, ","));
+		sb.append(") values (");
+		for (int i = 0; i < properties.size(); i++) {
+			sb.append(":").append(properties.get(i));
+			if (i < properties.size() - 1)
+				sb.append(",");
+		}
+		sb.append(")");
+		return sb.toString();
+	}
+
+	public static String buildUpdateStatement(String tableName, List<String> setColumns, List<String> whereColumns) {
+		StringBuilder sb = new StringBuilder("update ");
+		sb.append(tableName);
+		sb.append(" set ");
+		for (int i = 0; i < setColumns.size(); i++) {
+			String name = setColumns.get(i);
+			sb.append(name).append("=:").append(name);
+			if (i < setColumns.size() - 1)
+				sb.append(",");
+		}
+		if (whereColumns != null && !whereColumns.isEmpty()) {
+			sb.append(" where ");
+			for (int i = 0; i < whereColumns.size(); i++) {
+				String name = whereColumns.get(i);
+				sb.append(name).append("=:").append(name);
+				if (i < whereColumns.size() - 1)
+					sb.append(" and ");
+			}
+		}
+		return sb.toString();
+	}
 
 	public static String trim(String sql) {
 		sql = sql.trim();
