22ef7ec0e6b787438190930ffcd83030c5d4d00d
Yanming Zhou <zhouyanming@gmail.com>
2016-03-10 14:53:02 +0800

    Polishing

--- src/org/ironrhino/core/sequence/cyclic/RedisCyclicSequence.java
+++ src/org/ironrhino/core/sequence/cyclic/RedisCyclicSequence.java
@@ -43,41 +43,24 @@ public class RedisCyclicSequence extends AbstractCyclicSequence {
 	public String nextStringValue() {
 		long value = boundValueOperations.increment(1);
 		final String stringValue = String.valueOf(value);
-		if (stringValue.length() < getPaddingLength() + getCycleType().getPattern().length()) {
-			final String restart = getStringValue(now(), getPaddingLength(), 1);
-			boolean success = stringRedisTemplate.execute(new SessionCallback<Boolean>() {
-				@Override
-				@SuppressWarnings({ "unchecked", "rawtypes" })
-				public Boolean execute(RedisOperations operations) {
-					operations.watch(Collections.singleton(boundValueOperations.getKey()));
-					if (stringValue.equals(boundValueOperations.get())) {
-						operations.multi();
-						boundValueOperations.set(restart);
-						return operations.exec() != null;
-					} else {
-						return false;
-					}
-				}
-			});
-			return success ? restart : nextStringValue();
-		}
-		Calendar cal = Calendar.getInstance();
-		CycleType cycleType = getCycleType();
-		if (cycleType.ordinal() <= CycleType.MINUTE.ordinal())
-			cal.set(Calendar.MINUTE, Integer.valueOf(stringValue.substring(10, 12)));
-		if (cycleType.ordinal() <= CycleType.HOUR.ordinal())
-			cal.set(Calendar.HOUR_OF_DAY, Integer.valueOf(stringValue.substring(8, 10)));
-		if (cycleType.ordinal() <= CycleType.DAY.ordinal())
-			cal.set(Calendar.DAY_OF_MONTH, Integer.valueOf(stringValue.substring(6, 8)));
-		if (cycleType.ordinal() <= CycleType.MONTH.ordinal())
-			cal.set(Calendar.MONTH, Integer.valueOf(stringValue.substring(4, 6)) - 1);
-		if (cycleType.ordinal() <= CycleType.YEAR.ordinal())
-			cal.set(Calendar.YEAR, Integer.valueOf(stringValue.substring(0, 4)));
-		Date d = cal.getTime();
 		Date now = now();
-		if (getCycleType().isSameCycle(d, now))
-			return stringValue;
-
+		if (stringValue.length() == getPaddingLength() + getCycleType().getPattern().length()) {
+			Calendar cal = Calendar.getInstance();
+			CycleType cycleType = getCycleType();
+			if (cycleType.ordinal() <= CycleType.MINUTE.ordinal())
+				cal.set(Calendar.MINUTE, Integer.valueOf(stringValue.substring(10, 12)));
+			if (cycleType.ordinal() <= CycleType.HOUR.ordinal())
+				cal.set(Calendar.HOUR_OF_DAY, Integer.valueOf(stringValue.substring(8, 10)));
+			if (cycleType.ordinal() <= CycleType.DAY.ordinal())
+				cal.set(Calendar.DAY_OF_MONTH, Integer.valueOf(stringValue.substring(6, 8)));
+			if (cycleType.ordinal() <= CycleType.MONTH.ordinal())
+				cal.set(Calendar.MONTH, Integer.valueOf(stringValue.substring(4, 6)) - 1);
+			if (cycleType.ordinal() <= CycleType.YEAR.ordinal())
+				cal.set(Calendar.YEAR, Integer.valueOf(stringValue.substring(0, 4)));
+			Date d = cal.getTime();
+			if (getCycleType().isSameCycle(d, now))
+				return stringValue;
+		}
 		final String restart = getStringValue(now, getPaddingLength(), 1);
 		boolean success = stringRedisTemplate.execute(new SessionCallback<Boolean>() {
 			@Override
