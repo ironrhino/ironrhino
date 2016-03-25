aebf1eceb9883d5e97d4e183ea926874546928dc
Yanming Zhou <zhouyanming@gmail.com>
2016-03-25 17:32:04 +0800

    Override ParametersInteceptor add autoGrowCollectionLimit

---
+++ src/org/ironrhino/core/struts/ParamsInterceptor.java
@@ -0,0 +1,44 @@
+package org.ironrhino.core.struts;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.opensymphony.xwork2.interceptor.ParametersInterceptor;
+import com.opensymphony.xwork2.util.logging.Logger;
+import com.opensymphony.xwork2.util.logging.LoggerFactory;
+
+public class ParamsInterceptor extends ParametersInterceptor {
+
+	private static final long serialVersionUID = -9206985757047973716L;
+
+	private static final Logger LOG = LoggerFactory.getLogger(ParametersInterceptor.class);
+
+	protected int autoGrowCollectionLimit = 255;
+
+	public void setAutoGrowCollectionLimit(int autoGrowCollectionLimit) {
+		this.autoGrowCollectionLimit = autoGrowCollectionLimit;
+	}
+
+	@Override
+	protected boolean acceptableName(String name) {
+		boolean b = super.acceptableName(name);
+		if (b) {
+			int i = name.indexOf('[');
+			while (i > 0) {
+				int end = name.indexOf(']', i);
+				if (end < 0)
+					break;
+				String s = name.substring(i + 1, end);
+				if (StringUtils.isNumeric(s)) {
+					int index = Integer.valueOf(s);
+					if (index > autoGrowCollectionLimit) {
+						LOG.warn("Parameter \"#0\" exceed max index: [#1]", name, autoGrowCollectionLimit);
+						return false;
+					}
+				}
+				i = name.indexOf('[', end);
+			}
+		}
+		return b;
+	}
+
+}
