7f57e7fa4d09572301bdf13cd6d54d60c723b930
Yanming Zhou <zhouyanming@gmail.com>
2016-06-21 13:07:11 +0800

    Fix wrong import

--- src/org/ironrhino/core/struts/ValidationException.java
+++ src/org/ironrhino/core/struts/ValidationException.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.elasticsearch.common.lang3.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.opensymphony.xwork2.ValidationAwareSupport;
 
