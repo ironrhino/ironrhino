e9d22d2c3802f096e3c4a4732502494180661cf2
Yanming Zhou <zhouyanming@gmail.com>
2016-08-13 10:12:29 +0800

    Revert deprecated org.ironrhino.core.model.Entity

---
+++ src/org/ironrhino/core/model/Entity.java
@@ -0,0 +1,10 @@
+package org.ironrhino.core.model;
+
+import java.io.Serializable;
+
+@Deprecated
+public abstract class Entity<PK extends Serializable> extends AbstractEntity<PK> {
+
+	private static final long serialVersionUID = 7896344339483262576L;
+
+}
