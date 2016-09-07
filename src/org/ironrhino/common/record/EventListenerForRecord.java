56ec7eb1e14554bd30db4d7d57d9e48e3a282c3e
Yanming Zhou <zhouyanming@gmail.com>
2016-09-07 09:29:32 +0800

    Polishing

--- src/org/ironrhino/common/record/EventListenerForRecord.java
+++ src/org/ironrhino/common/record/EventListenerForRecord.java
@@ -7,33 +7,39 @@ import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.event.spi.PostUpdateEvent;
 import org.hibernate.event.spi.PostUpdateEventListener;
 import org.hibernate.persister.entity.EntityPersister;
+import org.ironrhino.core.util.ReflectionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EventListenerForRecord implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
+public class EventListenerForRecord
+		implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
 
 	private static final long serialVersionUID = 6292673725187749565L;
 
 	@Override
 	public void onPostDelete(PostDeleteEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(RecordAware.class))
+		if (isAnnotated(event.getEntity()))
 			RecordAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public void onPostInsert(PostInsertEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(RecordAware.class))
+		if (isAnnotated(event.getEntity()))
 			RecordAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public void onPostUpdate(PostUpdateEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(RecordAware.class))
+		if (isAnnotated(event.getEntity()))
 			RecordAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public boolean requiresPostCommitHanding(EntityPersister persister) {
 		return true;
+	}
+
+	private static boolean isAnnotated(Object entity) {
+		return ReflectionUtils.getActualClass(entity).isAnnotationPresent(RecordAware.class);
 	}
 }
--- src/org/ironrhino/core/aop/EventListenerForPublish.java
+++ src/org/ironrhino/core/aop/EventListenerForPublish.java
@@ -7,33 +7,39 @@ import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.event.spi.PostUpdateEvent;
 import org.hibernate.event.spi.PostUpdateEventListener;
 import org.hibernate.persister.entity.EntityPersister;
+import org.ironrhino.core.util.ReflectionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EventListenerForPublish implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
+public class EventListenerForPublish
+		implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
 
 	private static final long serialVersionUID = 9062685218966998574L;
 
 	@Override
 	public void onPostDelete(PostDeleteEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(PublishAware.class))
+		if (isAnnotated(event.getEntity()))
 			PublishAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public void onPostInsert(PostInsertEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(PublishAware.class))
+		if (isAnnotated(event.getEntity()))
 			PublishAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public void onPostUpdate(PostUpdateEvent event) {
-		if (event.getEntity().getClass().isAnnotationPresent(PublishAware.class))
+		if (isAnnotated(event.getEntity()))
 			PublishAspect.getHibernateEvents(true).add(event);
 	}
 
 	@Override
 	public boolean requiresPostCommitHanding(EntityPersister persister) {
 		return true;
+	}
+
+	private static boolean isAnnotated(Object entity) {
+		return ReflectionUtils.getActualClass(entity).isAnnotationPresent(PublishAware.class);
 	}
 }
