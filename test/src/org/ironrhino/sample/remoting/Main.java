16a05574989cf6eca193450b6a1b6b4b6bca790f
Yanming Zhou <zhouyanming@gmail.com>
2016-09-18 09:44:13 +0800

    Remove hessian

--- test/src/org/ironrhino/sample/remoting/Main.java
+++ test/src/org/ironrhino/sample/remoting/Main.java
@@ -10,18 +10,12 @@ public class Main {
 		AppInfo.initialize();
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"classpath*:org/ironrhino/sample/remoting/test_without_zk.xml");
-		UserService userServiceHessian = (UserService) ctx.getBean("userServiceHessian");
-		System.out.println(userServiceHessian.loadUserByUsername("admin").getUsername());
-		UserService userServiceHttpInvoker = (UserService) ctx.getBean("userServiceHttpInvoker");
-		System.out.println(userServiceHttpInvoker.loadUserByUsername("admin").getUsername());
+		UserService userService = ctx.getBean(UserService.class);
+		System.out.println(userService.loadUserByUsername("admin").getUsername());
 		int loop = 1000;
 		long time = System.currentTimeMillis();
 		for (int i = 0; i < loop; i++)
-			userServiceHessian.loadUserByUsername("admin");
-		System.out.println("hessian:" + (System.currentTimeMillis() - time) + "ms");
-		time = System.currentTimeMillis();
-		for (int i = 0; i < loop; i++)
-			userServiceHttpInvoker.loadUserByUsername("admin");
+			userService.loadUserByUsername("admin");
 		System.out.println("httpinvoker:" + (System.currentTimeMillis() - time) + "ms");
 		time = System.currentTimeMillis();
 		ctx.close();
