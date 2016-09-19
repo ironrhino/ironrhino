8f6baf5b225a87b0a75cba154a72cee5f356ef95
Yanming Zhou <zhouyanming@gmail.com>
2016-09-19 09:54:36 +0800

    Rename package of MixedPasswordEncoder

--- src/org/ironrhino/core/spring/security/MixedPasswordEncoder.java
+++ src/org/ironrhino/core/spring/security/MixedPasswordEncoder.java
@@ -1,25 +1,6 @@
 package org.ironrhino.core.spring.security;
 
-import org.ironrhino.core.util.CodecUtils;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public class MixedPasswordEncoder implements PasswordEncoder {
-
-	@Override
-	public String encode(CharSequence rawPassword) {
-		if (rawPassword == null)
-			return null;
-		return CodecUtils.digest(rawPassword.toString());
-	}
-
-	@Override
-	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		if (rawPassword == null)
-			rawPassword = "";
-		if (encodedPassword == null)
-			encodedPassword = "";
-		rawPassword = encode(rawPassword);
-		return encodedPassword.equals(rawPassword);
-	}
+@Deprecated
+public class MixedPasswordEncoder extends org.ironrhino.core.spring.security.password.MixedPasswordEncoder {
 
 }
