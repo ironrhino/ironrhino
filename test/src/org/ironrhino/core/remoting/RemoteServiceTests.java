39b589526ecdca2d832b5c2e73fa6b86cf4aff64
Yanming Zhou <zhouyanming@gmail.com>
2016-11-24 16:59:08 +0800

    Add RemotingServiceTests

---
+++ test/src/org/ironrhino/core/remoting/RemoteServiceTests.java
@@ -0,0 +1,102 @@
+package org.ironrhino.core.remoting;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.ironrhino.sample.remoting.TestService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "ctx.xml" })
+public class RemoteServiceTests {
+
+	public static final int THREADS = 100;
+
+	public static final int LOOP = 100;
+
+	private static ExecutorService executorService;
+
+	@Autowired
+	private TestService testService;
+
+	@BeforeClass
+	public static void setup() {
+		executorService = Executors.newFixedThreadPool(THREADS);
+	}
+
+	@AfterClass
+	public static void destroy() {
+		executorService.shutdown();
+	}
+
+	@Test
+	public void testEcho() {
+		testService.ping();
+		assertEquals("", testService.echo());
+		assertNull(testService.echo(null));
+		assertEquals("test", testService.echo("test"));
+		assertEquals(Collections.singletonList("list"), testService.echoList(Collections.singletonList("list")));
+		assertTrue(Arrays.equals(new String[] { "echoWithArrayList" },
+				testService.echoArray(new String[] { "echoWithArrayList" })));
+		assertEquals(3, testService.countAndAdd(Collections.singletonList("test"), 2));
+		assertNull(testService.loadUserByUsername(null));
+		assertEquals("username", testService.loadUserByUsername("username").getUsername());
+		assertNull(testService.search(null));
+		assertEquals(Collections.EMPTY_LIST, testService.search(""));
+		assertEquals("username", testService.search("username").get(0).getUsername());
+	}
+
+	@Test
+	public void testUserDetails() {
+		assertNull(testService.loadUserByUsername(null));
+		assertEquals("username", testService.loadUserByUsername("username").getUsername());
+		assertNull(testService.search(null));
+		assertEquals(Collections.EMPTY_LIST, testService.search(""));
+		assertEquals("username", testService.search("username").get(0).getUsername());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testOnException() throws Exception {
+		testService.throwException("this is a message");
+	}
+
+	@Test
+	public void testConcurreny() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(THREADS);
+		final AtomicInteger count = new AtomicInteger();
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < THREADS; i++) {
+
+			executorService.execute(new Runnable() {
+				@Override
+				public void run() {
+					for (int j = 0; j < LOOP; j++) {
+						assertEquals("test" + j, testService.echo("test" + j));
+						count.incrementAndGet();
+					}
+					cdl.countDown();
+				}
+			});
+		}
+		cdl.await();
+		time = System.currentTimeMillis() - time;
+		System.out.println("completed " + count.get() + " requests with concurrency(" + THREADS + ") in " + time
+				+ "ms (tps = " + (count.get() * 1000 / time) + ") with tryLock()");
+		assertEquals(count.get(), THREADS * LOOP);
+	}
+
+}
