51bf4fa887b75c147032f4d111640620d63f6f55
Yanming Zhou <zhouyanming@gmail.com>
2016-12-27 17:03:41 +0800

    Improve suggestion

--- webapp/assets/scripts/source/ironrhino.suggestion.js
+++ webapp/assets/scripts/source/ironrhino.suggestion.js
@@ -1,26 +1,33 @@
 Observation.suggestion = function(container) {
 	$$('input.suggestion', container).each(function() {
 				var t = $(this);
-				t.typeahead({
-							minLength : 2,
-							items : 20,
-							source : function(query, process) {
-								$.ajax({
-											global : false,
-											url : t.data('source'),
-											type : 'GET',
-											dataType : "json",
-											data : {
-												keyword : query
-											},
-											success : function(data) {
-												process(data);
-											}
-										});
-							},
-							matcher : function(item) {
-								return true;
-							}
-						});
+				var source = t.data('source');
+				if (source) {
+					if (source.indexOf('[') == 0)
+						source = JSON.parse(source.replace(/'/g, '"'));
+					else
+						source = function(query, process) {
+							$.ajax({
+										global : false,
+										url : t.data('source'),
+										type : 'GET',
+										dataType : "json",
+										data : {
+											keyword : query
+										},
+										success : function(data) {
+											process(data);
+										}
+									});
+						}
+					t.typeahead({
+								minLength : 2,
+								items : 20,
+								source : source,
+								matcher : function(item) {
+									return true;
+								}
+							});
+				}
 			});
 };
