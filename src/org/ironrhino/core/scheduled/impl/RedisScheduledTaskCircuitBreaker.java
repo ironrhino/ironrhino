bb73e180175ed1c3bd8ad46ca57449c0c40a6624
Yanming Zhou <zhouyanming@gmail.com>
2017-04-24 15:34:07 +0800

    Replace Boolean with String for human readable

--- src/org/ironrhino/core/scheduled/impl/RedisScheduledTaskCircuitBreaker.java
+++ src/org/ironrhino/core/scheduled/impl/RedisScheduledTaskCircuitBreaker.java
@@ -5,9 +5,9 @@ import static org.ironrhino.core.metadata.Profiles.CLUSTER;
 import static org.ironrhino.core.metadata.Profiles.DUAL;
 
 import org.ironrhino.core.scheduled.ScheduledTaskCircuitBreaker;
-import org.ironrhino.core.spring.configuration.PriorityQualifier;
 import org.ironrhino.core.spring.configuration.ServiceImplementationConditional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -18,20 +18,20 @@ public class RedisScheduledTaskCircuitBreaker implements ScheduledTaskCircuitBre
 	private static final String NAMESPACE_SHORT_CIRCUIT = "ShortCircuit:";
 
 	@Autowired
-	@PriorityQualifier("cacheRedisTemplate")
-	private RedisTemplate<String, Object> redisTemplate;
+	@Qualifier("stringRedisTemplate")
+	private RedisTemplate<String, String> stringRedisTemplate;
 
 	@Override
 	public boolean isShortCircuit(String task) {
-		Boolean b = (Boolean) redisTemplate.opsForValue().get(NAMESPACE_SHORT_CIRCUIT + task);
-		return b != null && b;
+		String s = stringRedisTemplate.opsForValue().get(NAMESPACE_SHORT_CIRCUIT + task);
+		return s != null && s.equals("true");
 	}
 
 	@Override
 	public void setShortCircuit(String task, boolean value) {
 		if (!value)
-			redisTemplate.delete(NAMESPACE_SHORT_CIRCUIT + task);
+			stringRedisTemplate.delete(NAMESPACE_SHORT_CIRCUIT + task);
 		else
-			redisTemplate.opsForValue().set(NAMESPACE_SHORT_CIRCUIT + task, value);
+			stringRedisTemplate.opsForValue().set(NAMESPACE_SHORT_CIRCUIT + task, String.valueOf(value));
 	}
 }
