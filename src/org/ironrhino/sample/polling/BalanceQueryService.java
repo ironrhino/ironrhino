92b47b0fae7f1c62478998bfd31deff593976b5f
Yanming Zhou <zhouyanming@gmail.com>
2017-06-26 17:20:42 +0800

    Disable component

--- src/org/ironrhino/sample/polling/BalanceQueryService.java
+++ src/org/ironrhino/sample/polling/BalanceQueryService.java
@@ -15,9 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Profile(Profiles.DUAL)
 public class BalanceQueryService {
 
