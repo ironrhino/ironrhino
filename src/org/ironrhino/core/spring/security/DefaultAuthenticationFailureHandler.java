d4ace4d07392b37f5f3aa00e59c28d842d25ae96
Yanming Zhou <zhouyanming@gmail.com>
2017-07-05 18:25:26 +0800

    Improve security from brute-force password cracking

--- src/org/ironrhino/core/spring/security/DefaultAuthenticationFailureHandler.java
+++ src/org/ironrhino/core/spring/security/DefaultAuthenticationFailureHandler.java
@@ -1,25 +1,48 @@
 package org.ironrhino.core.spring.security;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.ironrhino.core.cache.CacheManager;
 import org.ironrhino.core.spring.configuration.ResourcePresentConditional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 @ResourcePresentConditional("classpath*:resources/spring/applicationContext-security*.xml")
 public class DefaultAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
+	@Autowired
+	private UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter;
+
+	@Autowired
+	private CacheManager cacheManager;
+
+	@Value("${authenticationFailureHandler.delayNamespace:username}")
+	private String delayNamespace;
+
+	@Value("${authenticationFailureHandler.delayInterval:5}")
+	private int delayInterval;
+
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException e) throws IOException, ServletException {
 		request.getSession().removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		if (e instanceof BadCredentialsException) {
+			String username = request.getParameter(usernamePasswordAuthenticationFilter.getUsernameParameter());
+			if (username != null)
+				cacheManager.delay(username, delayNamespace, delayInterval, TimeUnit.SECONDS, false);
+		}
 	}
 
 }
