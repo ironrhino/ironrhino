2e71737bd255d5202e41139ece48d60b96208d7e
Yanming Zhou <zhouyanming@gmail.com>
2017-07-27 15:57:51 +0800

    Provide configuration "sitemesh.decorator.default"

---
+++ src/org/ironrhino/core/servlet/handles/SitemeshHandler.java
@@ -0,0 +1,25 @@
+package org.ironrhino.core.servlet.handles;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ironrhino.core.servlet.AccessHandler;
+import org.ironrhino.core.spring.configuration.ApplicationContextPropertiesConditional;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@ApplicationContextPropertiesConditional(key = "sitemesh.decorator.default", value = ApplicationContextPropertiesConditional.ANY)
+public class SitemeshHandler extends AccessHandler {
+
+	@Value("${sitemesh.decorator.default:}")
+	private String decorator;
+
+	@Override
+	public boolean handle(HttpServletRequest request, HttpServletResponse response) {
+		if (!decorator.isEmpty())
+			request.setAttribute("decorator", decorator);
+		return false;
+	}
+
+}
