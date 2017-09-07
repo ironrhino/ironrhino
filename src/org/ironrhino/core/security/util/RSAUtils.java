c9e3dca8a5ad755d6cce063e5321ee53844946a0
Yanming Zhou <zhouyanming@gmail.com>
2017-09-07 17:19:05 +0800

    Add RSAUtils

---
+++ src/org/ironrhino/core/security/util/RSAUtils.java
@@ -0,0 +1,143 @@
+package org.ironrhino.core.security.util;
+
+import java.io.ByteArrayOutputStream;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
+/*
+ * $ openssl 
+ * OpenSSL> genrsa -out rsa_private_key.pem 1024 
+ * OpenSSL> pkcs8 -topk8 -inform PEM -in rsa_private_key.pem -outform PEM -nocrypt -out rsa_private_key_pkcs8.pem // privateKey
+ * OpenSSL> rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem // publicKey
+ */
+public class RSAUtils {
+
+	public static final String KEY_ALGORTHM = "RSA";
+	public static final String SPECIFIC_KEY_ALGORITHM = "RSA/ECB/PKCS1Padding";
+	public static final String SIGNATURE_ALGORITHM = "SHA1WithRSA";
+	public static final String CHARSET = "UTF-8";
+
+	public static String encrypt(String data, String publicKey) throws Exception {
+		return Base64.getEncoder().encodeToString(encryptByPublicKey(data.getBytes(CHARSET), publicKey));
+	}
+
+	public static String decrypt(String data, String privateKey) throws Exception {
+		return new String(decryptByPrivateKey(Base64.getDecoder().decode(data), privateKey), CHARSET);
+	}
+
+	public static String sign(String data, String privateKey) throws Exception {
+		return Base64.getEncoder().encodeToString(signWithPrivateKey(data.getBytes(CHARSET), privateKey));
+	}
+
+	public static boolean verify(String data, String publicKey, String sign) throws Exception {
+		return verifyWithPublicKey(data.getBytes(CHARSET), publicKey, sign);
+	}
+
+	public static byte[] encryptByPublicKey(byte[] data, String key) throws Exception {
+		byte[] encryptedData = null;
+		byte[] keyBytes = Base64.getDecoder().decode(key.replaceAll("\\s", ""));
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+		Key publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+		Cipher cipher = Cipher.getInstance(SPECIFIC_KEY_ALGORITHM);
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		int maxEncryptBlockSize = getMaxEncryptBlockSize(keyFactory, publicKey);
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		try {
+			int dataLength = data.length;
+			for (int i = 0; i < data.length; i += maxEncryptBlockSize) {
+				int encryptLength = dataLength - i < maxEncryptBlockSize ? dataLength - i : maxEncryptBlockSize;
+				byte[] doFinal = cipher.doFinal(data, i, encryptLength);
+				bout.write(doFinal);
+			}
+			encryptedData = bout.toByteArray();
+		} finally {
+			if (bout != null) {
+				bout.close();
+			}
+		}
+		return encryptedData;
+	}
+
+	public static byte[] decryptByPrivateKey(byte[] data, String key) throws Exception {
+		byte[] decryptedData = null;
+		byte[] keyBytes = Base64.getDecoder().decode(key.replaceAll("\\s", ""));
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+		Key privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+		Cipher cipher = Cipher.getInstance(SPECIFIC_KEY_ALGORITHM);
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		int maxDecryptBlockSize = getMaxDecryptBlockSize(keyFactory, privateKey);
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		try {
+			int dataLength = data.length;
+			for (int i = 0; i < dataLength; i += maxDecryptBlockSize) {
+				int decryptLength = dataLength - i < maxDecryptBlockSize ? dataLength - i : maxDecryptBlockSize;
+				byte[] doFinal = cipher.doFinal(data, i, decryptLength);
+				bout.write(doFinal);
+			}
+			decryptedData = bout.toByteArray();
+		} finally {
+			if (bout != null) {
+				bout.close();
+			}
+		}
+		return decryptedData;
+	}
+
+	public static byte[] signWithPrivateKey(byte[] data, String key) throws Exception {
+		byte[] keyBytes = Base64.getDecoder().decode(key.replaceAll("\\s", ""));
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+		PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		signature.initSign(privateKey);
+		signature.update(data);
+		return signature.sign();
+	}
+
+	public static boolean verifyWithPublicKey(byte[] data, String key, String sign) throws Exception {
+		byte[] keyBytes = Base64.getDecoder().decode(key.replaceAll("\\s", ""));
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(keyBytes);
+		KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORTHM);
+		PublicKey publickey = keyFactory.generatePublic(x509EncodedKeySpec);
+		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		signature.initVerify(publickey);
+		signature.update(data);
+		return signature.verify(Base64.getDecoder().decode(sign));
+	}
+
+	private static int getMaxEncryptBlockSize(KeyFactory keyFactory, Key key) throws Exception {
+		int maxLength = 117;
+		try {
+			RSAPublicKeySpec publicKeySpec = (RSAPublicKeySpec) keyFactory.getKeySpec(key, RSAPublicKeySpec.class);
+			int keyLength = publicKeySpec.getModulus().bitLength();
+			maxLength = keyLength / 8 - 11;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return maxLength;
+	}
+
+	private static int getMaxDecryptBlockSize(KeyFactory keyFactory, Key key) throws Exception {
+		int maxLength = 128;
+		try {
+			RSAPrivateKeySpec publicKeySpec = (RSAPrivateKeySpec) keyFactory.getKeySpec(key, RSAPrivateKeySpec.class);
+			int keyLength = publicKeySpec.getModulus().bitLength();
+			maxLength = keyLength / 8;
+		} catch (Exception e) {
+		}
+		return maxLength;
+	}
+
+}
---
+++ test/src/org/ironrhino/core/security/util/RSAUtilsTest.java
@@ -0,0 +1,47 @@
+package org.ironrhino.core.security.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RSAUtilsTest {
+
+	public static final String PUBLIC_KEY = 
+			"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCyQUq3Kj6pJfPkAACsoF4laMzq\n" + 
+			"ZCej+2lIlW64ZKi1jVL8/xenrpCpnh2qBvP6y8Kx66WbwoS//8R8hKkRiHgRkEkx\n" + 
+			"8WRLYxfJiTIi/gWC08XbPUmQq3otFFHFnJ6dDoggn2ts3AeIu8Ag2iFv821bqc2y\n" + 
+			"QcOlUY7HFVOQfPcMoQIDAQAB";
+	public static final String PRIVATE_KEY = 
+			"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALJBSrcqPqkl8+QA\n" + 
+			"AKygXiVozOpkJ6P7aUiVbrhkqLWNUvz/F6eukKmeHaoG8/rLwrHrpZvChL//xHyE\n" + 
+			"qRGIeBGQSTHxZEtjF8mJMiL+BYLTxds9SZCrei0UUcWcnp0OiCCfa2zcB4i7wCDa\n" + 
+			"IW/zbVupzbJBw6VRjscVU5B89wyhAgMBAAECgYAdwuT0m+sGVr3XrWBvcf8GA+9i\n" + 
+			"mwI7ULiNU9W+l5/LYCMg9n4+kti4WRvezXZiwy5ogk2OKfX8EHn/yC5qLPzOulSI\n" + 
+			"JD3ipQtEdcctKAYKuiM4pCKxbKTS+4xhRLGVIdkTdemv1XIZTvMvQbku7beLHKaZ\n" + 
+			"iwZ+ILdAGBWKcDMq2QJBAOfDN8/AZ8VcqFyuNEmYHpAs85ZjthY4atHeCqtZXWXY\n" + 
+			"Khzne6gksVRoCNmRYY2uf5rM4gelI9SRuzzPuJpatusCQQDE5Y/9GEmN9s0cVXIp\n" + 
+			"qRS2kR6B5pzhMdu0r8rToZLeZmAAx9f0h/QQEK8eo8mLiuahDF3UNHidwe3GIlzr\n" + 
+			"4n+jAkEA5czFjDNFMYZcUflRUx+IPoCzzoxzwbiTAiDeB2SGnTMnHp3QGLq5Me4t\n" + 
+			"yDzEs80wLXe1LstpqZ4OP4/fhP0pnQJAdo1SWS3ae+PyM/Euv+7STeqq18QnCWCf\n" + 
+			"zPdbxHkwmUBC5bVuTgnd5h5lkqlDnQWRP77WcAL37OTrabUfBdhmyQJAVWhO72t3\n" + 
+			"ISuLYaDhyQ98jnrJDhgYt1GCzwLrkkOlPhsOhfpX6NFgr3UrIZnAjKd9F3MCPmA1\n" + 
+			"AnVrwY9YU+ZqoQ==";
+
+	@Test
+	public void testEncrypt() throws Exception {
+		String data = "I am test";
+		String encryptedData = RSAUtils.encrypt(data, PUBLIC_KEY);
+		assertNotEquals(data, encryptedData);
+		String decryptedData = RSAUtils.decrypt(encryptedData, PRIVATE_KEY);
+		assertEquals(data, decryptedData);
+	}
+
+	@Test
+	public void testSign() throws Exception {
+		String data = "I am test";
+		String sign = RSAUtils.sign(data, PRIVATE_KEY);
+		assertTrue(RSAUtils.verify(data, PUBLIC_KEY, sign));
+	}
+}
