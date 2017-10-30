7b35a1d37d7b6dd973362b93916d3a9e47d34bab
Yanming Zhou <zhouyanming@gmail.com>
2017-10-30 09:30:20 +0800

    Use ApiConfig version for servlet mapping

--- src/org/ironrhino/sample/api/AppInitializer.java
+++ src/org/ironrhino/sample/api/AppInitializer.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ironrhino.core.servlet.DelegatingFilter;
 import org.ironrhino.core.spring.servlet.InheritedDispatcherServlet;
 import org.springframework.web.WebApplicationInitializer;
@@ -18,16 +19,18 @@ public class AppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-
-		ServletRegistration.Dynamic dynamic = servletContext.addServlet("api", InheritedDispatcherServlet.class);
+		String version = new ApiConfig().getVersion();
+		String servletName = "api" + version;
+		String servletMapping = "/api" + (StringUtils.isNotBlank(version) ? "/" + version : "") + "/*";
+		ServletRegistration.Dynamic dynamic = servletContext.addServlet(servletName, InheritedDispatcherServlet.class);
 		dynamic.setInitParameter(ContextLoader.CONTEXT_CLASS_PARAM,
 				AnnotationConfigWebApplicationContext.class.getName());
 		dynamic.setInitParameter(ContextLoader.CONFIG_LOCATION_PARAM, ApiConfig.class.getName());
-		dynamic.addMapping("/api/*");
+		dynamic.addMapping(servletMapping);
 		dynamic.setAsyncSupported(true);
 		dynamic.setLoadOnStartup(1);
 		FilterRegistration.Dynamic dynamicFilter = servletContext.addFilter("restFilter", DelegatingFilter.class);
 		dynamicFilter.setAsyncSupported(true);
-		dynamicFilter.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST), true, "api");
+		dynamicFilter.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST), true, servletName);
 	}
 }
