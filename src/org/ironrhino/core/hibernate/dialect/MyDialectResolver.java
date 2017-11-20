d91c158c7ccae99d8cf05892a5d7b67496fa463a
Yanming Zhou <zhouyanming@gmail.com>
2017-11-20 15:06:13 +0800

    Resolve Dialect for PostgreSQL 10

--- src/org/ironrhino/core/hibernate/dialect/MyDialectResolver.java
+++ src/org/ironrhino/core/hibernate/dialect/MyDialectResolver.java
@@ -2,6 +2,7 @@ package org.ironrhino.core.hibernate.dialect;
 
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.MySQL57InnoDBDialect;
+import org.hibernate.dialect.PostgreSQL94Dialect;
 import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
 import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
 import org.ironrhino.core.jdbc.DatabaseProduct;
@@ -13,14 +14,17 @@ public class MyDialectResolver extends StandardDialectResolver {
 	@Override
 	public Dialect resolveDialect(DialectResolutionInfo info) {
 		String databaseName = info.getDatabaseName();
+		int majorVersion = info.getDatabaseMajorVersion();
+		int minorVersion = info.getDatabaseMinorVersion();
 		DatabaseProduct database = DatabaseProduct.parse(databaseName);
 		if (database == DatabaseProduct.MYSQL) {
-			int majorVersion = info.getDatabaseMajorVersion();
-			int minorVersion = info.getDatabaseMinorVersion();
 			if (majorVersion > 5 || majorVersion == 5 && minorVersion >= 7)
 				return new MySQL57InnoDBDialect();
 			else if (majorVersion == 5 && minorVersion >= 6)
 				return new MySQL56Dialect();
+		} else if (database == DatabaseProduct.POSTGRESQL) {
+			if (majorVersion >= 10)
+				return new PostgreSQL94Dialect();
 		}
 		return super.resolveDialect(info);
 	}
