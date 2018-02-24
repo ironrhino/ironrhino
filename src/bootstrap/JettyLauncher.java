cab633b29cdf82ce6b1ed2b8011c120cbdf32d69
Yanming Zhou <zhouyanming@gmail.com>
2018-02-24 11:06:58 +0800

    Disable session management for jetty

--- src/bootstrap/JettyLauncher.java
+++ src/bootstrap/JettyLauncher.java
@@ -2,8 +2,14 @@ package bootstrap;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.SessionIdManager;
+import org.eclipse.jetty.server.session.HouseKeeper;
+import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletHandler.Default404Servlet;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -18,6 +24,7 @@ public class JettyLauncher {
 		if (p != null && p.trim().length() > 0)
 			port = Integer.valueOf(p);
 		Server server = new Server(port);
+		server.setSessionIdManager(new DummySessionIdManager());
 		Configuration.ClassList classlist = Configuration.ClassList.setServerDefault(server);
 		classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
 				"org.eclipse.jetty.annotations.AnnotationConfiguration");
@@ -43,6 +50,109 @@ public class JettyLauncher {
 		server.setStopAtShutdown(true);
 		server.start();
 		server.join();
+	}
+
+	static class DummySessionIdManager implements SessionIdManager {
+
+		@Override
+		public void start() throws Exception {
+		}
+
+		@Override
+		public void stop() throws Exception {
+		}
+
+		@Override
+		public boolean isRunning() {
+			return false;
+		}
+
+		@Override
+		public boolean isStarted() {
+			return false;
+		}
+
+		@Override
+		public boolean isStarting() {
+			return false;
+		}
+
+		@Override
+		public boolean isStopping() {
+			return false;
+		}
+
+		@Override
+		public boolean isStopped() {
+			return false;
+		}
+
+		@Override
+		public boolean isFailed() {
+			return false;
+		}
+
+		@Override
+		public void addLifeCycleListener(Listener listener) {
+		}
+
+		@Override
+		public void removeLifeCycleListener(Listener listener) {
+		}
+
+		@Override
+		public boolean isIdInUse(String id) {
+			return false;
+		}
+
+		@Override
+		public void expireAll(String id) {
+		}
+
+		@Override
+		public void invalidateAll(String id) {
+		}
+
+		@Override
+		public String newSessionId(HttpServletRequest request, long created) {
+			return null;
+		}
+
+		@Override
+		public String getWorkerName() {
+			return null;
+		}
+
+		@Override
+		public String getId(String extendedId) {
+			return null;
+		}
+
+		@Override
+		public String getExtendedId(String clusterId, HttpServletRequest request) {
+			return null;
+		}
+
+		@Override
+		public String renewSessionId(String oldClusterId, String oldNodeId, HttpServletRequest request) {
+			return null;
+		}
+
+		@Override
+		public Set<SessionHandler> getSessionHandlers() {
+			return null;
+		}
+
+		@Override
+		public void setSessionHouseKeeper(HouseKeeper houseKeeper) {
+
+		}
+
+		@Override
+		public HouseKeeper getSessionHouseKeeper() {
+			return null;
+		}
+
 	}
 
 }
