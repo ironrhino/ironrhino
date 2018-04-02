5922624509f1b29bbd2140b435f0278dd7febded
Yanming Zhou <zhouyanming@gmail.com>
2018-04-02 11:29:38 +0800

    Polishing

--- test/src/org/ironrhino/sample/remoting/Main.java
+++ test/src/org/ironrhino/sample/remoting/Main.java
@@ -1,15 +1,17 @@
 package org.ironrhino.sample.remoting;
 
+import org.ironrhino.core.remoting.client.HttpInvokerClient;
 import org.ironrhino.core.util.AppInfo;
 import org.ironrhino.security.service.UserService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class Main {
 
 	public static void main(String[] args) {
 		AppInfo.initialize();
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
-				"classpath*:org/ironrhino/sample/remoting/test.xml");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
 		UserService userService = ctx.getBean(UserService.class);
 		System.out.println(userService.loadUserByUsername("admin").getUsername());
 		int loop = 1000;
@@ -19,6 +21,19 @@ public class Main {
 		System.out.println("httpinvoker:" + (System.currentTimeMillis() - time) + "ms");
 		time = System.currentTimeMillis();
 		ctx.close();
+	}
+
+	@Configuration
+	static class Config {
+
+		@Bean
+		public HttpInvokerClient userService() {
+			HttpInvokerClient hic = new HttpInvokerClient();
+			hic.setServiceInterface(UserService.class);
+			hic.setHost("localhost");
+			return hic;
+		}
+
 	}
 
 }
