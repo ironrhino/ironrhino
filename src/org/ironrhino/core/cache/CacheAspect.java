817a0d095bf43e0adac0f57906b5c8f38aa5eb0f
Yanming Zhou <zhouyanming@gmail.com>
2018-05-09 13:50:24 +0800

    Switch Interceptor to @Aspect
    https://jira.spring.io/browse/SPR-16757
    https://jira.spring.io/browse/SPR-16803

--- src/org/ironrhino/core/cache/CacheAspect.java
+++ src/org/ironrhino/core/cache/CacheAspect.java
@@ -1,66 +1,151 @@
 package org.ironrhino.core.cache;
 
-import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import org.aopalliance.aop.Advice;
-import org.springframework.aop.Pointcut;
-import org.springframework.aop.support.AbstractPointcutAdvisor;
-import org.springframework.aop.support.StaticMethodMatcherPointcut;
+import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.ironrhino.core.aop.BaseAspect;
+import org.ironrhino.core.model.NullObject;
+import org.ironrhino.core.util.ExpressionUtils;
+import org.mvel2.PropertyAccessException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.BridgeMethodResolver;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-import lombok.Setter;
-
+@Aspect
 @Component
-public class CacheAspect extends AbstractPointcutAdvisor {
+public class CacheAspect extends BaseAspect {
 
-	private static final long serialVersionUID = -9093221616339043624L;
+	private final static String MUTEX = "_MUTEX_";
 
 	@Autowired
-	private ApplicationContext ctx;
+	private CacheManager cacheManager;
 
-	// lazy get CacheManager from ctx
-	// @Autowired
-	// private CacheManager cacheManager;
-
-	@Getter
-	@Setter
-	private int order = Ordered.HIGHEST_PRECEDENCE + 3;
-
-	private transient final StaticMethodMatcherPointcut pointcut = new StaticMethodMatcherPointcut() {
-		@Override
-		public boolean matches(Method method, Class<?> targetClass) {
-			method = BridgeMethodResolver.findBridgedMethod(method);
-			return method.getAnnotation(CheckCache.class) != null || method.getAnnotation(EvictCache.class) != null;
-		}
-	};
-
-	private volatile transient CacheInterceptor interceptor;
-
-	@Override
-	public Pointcut getPointcut() {
-		return pointcut;
+	public CacheAspect() {
+		order = Ordered.HIGHEST_PRECEDENCE + 3;
 	}
 
-	@Override
-	public Advice getAdvice() {
-		CacheInterceptor temp = interceptor;
-		if (temp == null) {
-			synchronized (this) {
-				temp = interceptor;
-				if (temp == null) {
-					temp = new CacheInterceptor();
-					CacheManager cacheManager = ctx.getBean(CacheManager.class);
-					temp.setCacheManager(cacheManager);
-					interceptor = temp;
+	@Around("execution(public * *(..)) and @annotation(checkCache)")
+	public Object get(ProceedingJoinPoint jp, CheckCache checkCache) throws Throwable {
+		if (isBypass())
+			return jp.proceed();
+		Map<String, Object> context = buildContext(jp);
+		String namespace = ExpressionUtils.evalString(checkCache.namespace(), context);
+		List<String> keys = ExpressionUtils.evalList(checkCache.key(), context);
+		if (keys == null || keys.isEmpty())
+			return jp.proceed();
+		String keyMutex = MUTEX + String.join("_", keys);
+		boolean mutexed = false;
+		if (CacheContext.isForceFlush()) {
+			cacheManager.mdelete(new HashSet<>(keys), namespace);
+		} else {
+			Class<?> returnType = ((MethodSignature) jp.getSignature()).getMethod().getReturnType();
+			int timeToIdle = ExpressionUtils.evalInt(checkCache.timeToIdle(), context, 0);
+			for (String key : keys) {
+				Object value = (timeToIdle > 0 && !cacheManager.supportsTti())
+						? cacheManager.getWithTti(key, namespace, timeToIdle, checkCache.timeUnit())
+						: cacheManager.get(key, namespace);
+				if (value instanceof NullObject) {
+					ExpressionUtils.eval(checkCache.onHit(), context);
+					return null;
+				}
+				if (value != null
+						&& (returnType.isPrimitive() && value.getClass() == ClassUtils.primitiveToWrapper(returnType)
+								|| returnType.isAssignableFrom(value.getClass()))) {
+					putReturnValueIntoContext(context, value);
+					ExpressionUtils.eval(checkCache.onHit(), context);
+					return value;
 				}
 			}
+			int throughPermits = checkCache.throughPermits();
+			int waitTimeout = checkCache.waitTimeout();
+			if (waitTimeout <= 0)
+				waitTimeout = 200;
+			else if (waitTimeout > 10000)
+				waitTimeout = 10000;
+			if (cacheManager.increment(keyMutex, 1, waitTimeout, TimeUnit.MILLISECONDS, namespace) <= throughPermits) {
+				mutexed = true;
+			} else {
+				Thread.sleep(waitTimeout);
+				for (String key : keys) {
+					Object value = cacheManager.get(key, namespace);
+					if (value instanceof NullObject) {
+						ExpressionUtils.eval(checkCache.onHit(), context);
+						return null;
+					}
+					if (value != null && (returnType.isPrimitive()
+							&& value.getClass() == ClassUtils.primitiveToWrapper(returnType)
+							|| returnType.isAssignableFrom(value.getClass()))) {
+						putReturnValueIntoContext(context, value);
+						ExpressionUtils.eval(checkCache.onHit(), context);
+						return value;
+					}
+				}
+			}
+			ExpressionUtils.eval(checkCache.onMiss(), context);
 		}
-		return temp;
+		Object result = jp.proceed();
+		putReturnValueIntoContext(context, result);
+		if (ExpressionUtils.evalBoolean(checkCache.when(), context, true)) {
+			Object cacheResult = (result == null && checkCache.cacheNull()) ? NullObject.get() : result;
+			if (cacheResult != null) {
+				if (checkCache.eternal()) {
+					for (String key : keys)
+						cacheManager.put(key, cacheResult, 0, checkCache.timeUnit(), namespace);
+				} else {
+					int timeToLive = ExpressionUtils.evalInt(checkCache.timeToLive(), context, 0);
+					int timeToIdle = ExpressionUtils.evalInt(checkCache.timeToIdle(), context, 0);
+					for (String key : keys)
+						if (timeToIdle > 0 && cacheManager.supportsTti())
+							cacheManager.putWithTti(key, cacheResult, timeToIdle, checkCache.timeUnit(), namespace);
+						else
+							cacheManager.put(key, cacheResult, timeToLive, checkCache.timeUnit(), namespace);
+				}
+			}
+			if (result != null)
+				ExpressionUtils.eval(checkCache.onPut(), context);
+		}
+		if (mutexed)
+			cacheManager.increment(keyMutex, -1, 0, TimeUnit.MILLISECONDS, namespace);
+		return result;
+	}
+
+	@Around("execution(public * *(..)) and @annotation(evictCache)")
+	public Object remove(ProceedingJoinPoint jp, EvictCache evictCache) throws Throwable {
+		Map<String, Object> context = buildContext(jp);
+		String namespace = ExpressionUtils.evalString(evictCache.namespace(), context);
+		boolean fallback = false;
+		List<String> keys = null;
+		try {
+			keys = ExpressionUtils.evalList(evictCache.key(), context);
+		} catch (PropertyAccessException e) {
+			fallback = true; // required retval
+		}
+		Object retval = jp.proceed();
+		putReturnValueIntoContext(context, retval);
+		if (fallback)
+			keys = ExpressionUtils.evalList(evictCache.key(), context);
+		if (isBypass() || keys == null || keys.size() == 0)
+			return retval;
+		cacheManager.mdelete(new HashSet<>(keys), namespace);
+		ExpressionUtils.eval(evictCache.onEvict(), context);
+		if (StringUtils.isNotBlank(evictCache.renew())) {
+			Object value = ExpressionUtils.eval(evictCache.renew(), context);
+			// keys may be changed, eval again
+			if (!fallback)
+				keys = ExpressionUtils.evalList(evictCache.key(), context);
+			for (Object key : keys)
+				if (key != null)
+					cacheManager.put(key.toString(), value, 0, TimeUnit.SECONDS, namespace);
+		}
+		return retval;
 	}
 
 }
--- src/org/ironrhino/sample/remoting/PersonRepositoryImpl.java
+++ src/org/ironrhino/sample/remoting/PersonRepositoryImpl.java
@@ -2,6 +2,7 @@ package org.ironrhino.sample.remoting;
 
 import java.util.List;
 
+import org.ironrhino.core.cache.CheckCache;
 import org.ironrhino.core.jdbc.JdbcRepository;
 import org.ironrhino.core.jdbc.Sql;
 import org.ironrhino.sample.crud.Person;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 public interface PersonRepositoryImpl extends PersonRepository {
 
 	@Override
+	@CheckCache(key = "personRepository.findAll()")
 	@Transactional(readOnly = true)
 	@Sql("select identityNo as \"id.identityNo\",identityType as \"id.identityType\",name,region_id as \"region.id\" from sample_person")
 	List<Person> findAll();
