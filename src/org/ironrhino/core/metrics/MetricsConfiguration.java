131551ff8864760b42ef658516701ee2cb2006eb
Yanming Zhou <zhouyanming@gmail.com>
2018-05-22 17:27:27 +0800

    Add TimedAspect

--- src/org/ironrhino/core/metrics/MetricsConfiguration.java
+++ src/org/ironrhino/core/metrics/MetricsConfiguration.java
@@ -14,6 +14,7 @@ import org.ironrhino.core.util.AppInfo;
 import org.ironrhino.core.util.NameableThreadFactory;
 import org.ironrhino.core.util.ReflectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.util.ClassUtils;
@@ -69,6 +70,11 @@ public class MetricsConfiguration {
 	@PreDestroy
 	public void destroy() {
 		Metrics.globalRegistry.getRegistries().forEach(MeterRegistry::close);
+	}
+
+	@Bean
+	public TimedAspect timedAspect() {
+		return new TimedAspect();
 	}
 
 	protected void instrument() {
---
+++ src/org/ironrhino/core/metrics/TimedAspect.java
@@ -0,0 +1,85 @@
+package org.ironrhino.core.metrics;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.ironrhino.core.spring.NameGenerator;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.LongTaskTimer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
+
+@Aspect
+public class TimedAspect {
+
+	@Around("execution(* *.*(..)) and @annotation(timed)")
+	public Object timing(ProceedingJoinPoint pjp, Timed timed) throws Throwable {
+		MeterRegistry registry = Metrics.globalRegistry;
+		String name = timed.value();
+		if (name.isEmpty()) {
+			StringBuilder sb = new StringBuilder("timed.");
+			Class<?> beanClass = pjp.getTarget().getClass();
+			String beanName = NameGenerator.buildDefaultBeanName(beanClass.getName());
+			Component comp = AnnotatedElementUtils.getMergedAnnotation(beanClass, Component.class);
+			if (comp != null && StringUtils.isNotBlank(comp.value()))
+				beanName = comp.value();
+			sb.append(beanName).append('.').append(pjp.getSignature().getName()).append("()");
+			name = sb.toString();
+		}
+		if (timed.longTask()) {
+			LongTaskTimer longTaskTimer = LongTaskTimer.builder(name).tags(timed.extraTags()).register(registry);
+			return recordThrowable(longTaskTimer, () -> recordThrowable(longTaskTimer, pjp::proceed));
+		} else {
+			Timer.Builder timerBuilder = Timer.builder(name).tags(timed.extraTags());
+			if (timed.histogram())
+				timerBuilder.publishPercentileHistogram();
+			if (timed.percentiles().length > 0)
+				timerBuilder = timerBuilder.publishPercentiles(timed.percentiles());
+			Timer timer = timerBuilder.register(registry);
+			return recordThrowable(timer, pjp::proceed);
+		}
+	}
+
+	@Around("execution(* *.*(..)) and @annotation(scheduled) and not @annotation(io.micrometer.core.annotation.Timed)")
+	public Object timing(ProceedingJoinPoint pjp, Scheduled scheduled) throws Throwable {
+		if (scheduled.cron().isEmpty())
+			return pjp.proceed();
+		Timed timed = AnnotationUtils.synthesizeAnnotation(Collections.singletonMap("longTask", true), Timed.class,
+				((MethodSignature) pjp.getSignature()).getMethod());
+		return timing(pjp, timed);
+	}
+
+	private Object recordThrowable(LongTaskTimer timer, ThrowableCallable f) throws Throwable {
+		LongTaskTimer.Sample timing = timer.start();
+		try {
+			return f.call();
+		} finally {
+			timing.stop();
+		}
+	}
+
+	private Object recordThrowable(Timer timer, ThrowableCallable f) throws Throwable {
+		MeterRegistry registry = Metrics.globalRegistry;
+		long start = registry.config().clock().monotonicTime();
+		try {
+			return f.call();
+		} finally {
+			timer.record(registry.config().clock().monotonicTime() - start, TimeUnit.NANOSECONDS);
+		}
+	}
+
+	private interface ThrowableCallable {
+		Object call() throws Throwable;
+	}
+}
