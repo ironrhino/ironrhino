320c859868088eb140492b3b1a382e40b51cf04e
Yanming Zhou <zhouyanming@gmail.com>
2018-05-28 17:38:32 +0800

    Replace DataRouteInterceptor with DataRouteAspect

--- src/org/ironrhino/core/dataroute/DataRouteAspect.java
+++ src/org/ironrhino/core/dataroute/DataRouteAspect.java
@@ -1,44 +1,60 @@
 package org.ironrhino.core.dataroute;
 
-import java.lang.reflect.Method;
+import java.util.Map;
 
-import org.aopalliance.aop.Advice;
-import org.springframework.aop.Pointcut;
-import org.springframework.aop.support.AbstractPointcutAdvisor;
-import org.springframework.aop.support.StaticMethodMatcherPointcut;
-import org.springframework.core.BridgeMethodResolver;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.ironrhino.core.aop.BaseAspect;
+import org.ironrhino.core.util.ExpressionUtils;
 
-import lombok.Getter;
-import lombok.Setter;
+@Aspect
+public class DataRouteAspect extends BaseAspect {
 
-public class DataRouteAspect extends AbstractPointcutAdvisor {
-
-	private static final long serialVersionUID = -9093221616339043624L;
-
-	@Getter
-	@Setter
-	private int order = -2;
-
-	private transient final StaticMethodMatcherPointcut pointcut = new StaticMethodMatcherPointcut() {
-		@Override
-		public boolean matches(Method method, Class<?> targetClass) {
-			method = BridgeMethodResolver.findBridgedMethod(method);
-			return AnnotationUtils.findAnnotation(method, DataRoute.class) != null
-					|| AnnotationUtils.findAnnotation(targetClass, DataRoute.class) != null;
-		}
-	};
-
-	private transient final DataRouteInterceptor interceptor = new DataRouteInterceptor();
-
-	@Override
-	public Pointcut getPointcut() {
-		return pointcut;
+	public DataRouteAspect() {
+		order = -2;
 	}
 
-	@Override
-	public Advice getAdvice() {
-		return interceptor;
+	@Around("execution(public * *(..)) and @annotation(dataRoute)")
+	public Object routeByMethod(ProceedingJoinPoint pjp, DataRoute dataRoute) throws Throwable {
+		Map<String, Object> context = buildContext(pjp);
+		String nodeName = ExpressionUtils.evalString(dataRoute.nodeName(), context);
+		Object routingKey = ExpressionUtils.eval(dataRoute.routingKey(), context);
+		String routerName = dataRoute.routerName();
+		boolean routingKeyPresent = !(routingKey == null
+				|| routingKey instanceof String && StringUtils.isBlank((String) routingKey));
+		if (routingKeyPresent) {
+			DataRouteContext.setRoutingKey(routingKey);
+			if (StringUtils.isNotBlank(routerName))
+				DataRouteContext.setRouterName(routerName);
+		} else {
+			if (StringUtils.isNotBlank(nodeName))
+				DataRouteContext.setNodeName(nodeName);
+		}
+		try {
+			return pjp.proceed();
+		} finally {
+			if (routingKeyPresent) {
+				DataRouteContext.removeRoutingKey(routingKey);
+				if (StringUtils.isNotBlank(routerName))
+					DataRouteContext.removeRouterName(routerName);
+			} else {
+				if (StringUtils.isNotBlank(nodeName))
+					DataRouteContext.removeNodeName(nodeName);
+			}
+		}
+	}
+
+	@Around("execution(public * *(..)) and @within(dataRoute) and not @annotation(org.ironrhino.core.dataroute.DataRoute)")
+	public Object routeByClass(ProceedingJoinPoint pjp, DataRoute dataRoute) throws Throwable {
+		String nodeName = dataRoute.nodeName();
+		DataRouteContext.setNodeName(nodeName);
+		try {
+			return pjp.proceed();
+		} finally {
+			DataRouteContext.removeNodeName(nodeName);
+		}
 	}
 
 }
