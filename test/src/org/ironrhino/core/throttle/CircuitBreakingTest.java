c3f02db6ce11297509c70d56308e6f3e47405cc1
Yanming Zhou <zhouyanming@gmail.com>
2018-07-06 18:22:24 +0800

    Use mock for tests

--- test/src/org/ironrhino/core/throttle/CircuitBreakingTest.java
+++ test/src/org/ironrhino/core/throttle/CircuitBreakingTest.java
@@ -1,41 +1,59 @@
 package org.ironrhino.core.throttle;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CircuitBreakingTest {
+
+	@Mock
+	EchoService echoService;
 
 	@Test(expected = CircuitBreakerOpenException.class)
 	public void test() throws Exception {
-		EchoService echoService = new EchoService();
-		for (int i = 0; i < 10000; i++) {
+		AtomicInteger count = new AtomicInteger();
+		when(echoService.echo(anyString())).thenAnswer(new Answer<String>() {
+			public String answer(InvocationOnMock invocation) throws IOException {
+				if (count.incrementAndGet() % 2 == 0)
+					throw new IOException("test");
+				Object[] args = invocation.getArguments();
+				return (String) args[0];
+			}
+		});
+		AtomicInteger success = new AtomicInteger();
+		AtomicInteger error = new AtomicInteger();
+		for (int i = 0; i < 101; i++) {
 			try {
-				echoService.echo("test");
+				CircuitBreaking.executeThrowableCallable(this.getClass().getName(), ex -> ex instanceof IOException,
+						() -> echoService.echo("test"));
+				assertTrue(success.get() == error.get());
+				success.incrementAndGet();
 			} catch (IOException e) {
-
+				error.incrementAndGet();
+				assertTrue(success.get() == error.get());
+			} finally {
+				assertTrue(success.get() <= 50);
+				assertTrue(error.get() <= 50);
 			}
 		}
 	}
 
-	public static class EchoService {
+	public static interface EchoService {
 
-		public AtomicInteger count = new AtomicInteger();
-
-		public String echo(String s) throws IOException {
-			return CircuitBreaking.executeThrowableCallable(this.getClass().getName(), ex -> ex instanceof IOException,
-					() -> doEcho(s));
-		}
-
-		private String doEcho(String s) throws IOException {
-			if (Math.abs(count.getAndIncrement()) % 2 == 1)
-				throw new IOException("for test");
-			return s;
-
-		}
+		public String echo(String s) throws IOException;
 
 	}
 
