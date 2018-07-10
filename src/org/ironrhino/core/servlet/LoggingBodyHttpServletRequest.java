86d006529b0b2b53ce4fa46d5f54a4a3ccb77cb1
Yanming Zhou <zhouyanming@gmail.com>
2018-07-10 16:01:18 +0800

    Improve LoggingBodyHttpServletRequest and LoggingBodyHttpServletResponse

--- src/org/ironrhino/core/servlet/LoggingBodyHttpServletRequest.java
+++ src/org/ironrhino/core/servlet/LoggingBodyHttpServletRequest.java
@@ -1,7 +1,9 @@
 package org.ironrhino.core.servlet;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
@@ -12,74 +14,124 @@ import org.ironrhino.core.util.AppInfo;
 import org.ironrhino.core.util.AppInfo.Stage;
 import org.ironrhino.core.util.JsonDesensitizer;
 import org.slf4j.Logger;
+import org.springframework.web.util.WebUtils;
 
+/**
+ * @See org.springframework.web.util.ContentCachingRequestWrapper
+ */
 public class LoggingBodyHttpServletRequest extends HttpServletRequestWrapper {
-
-	private Logger logger;
 
 	private volatile ServletInputStream servletInputStream;
 
-	private ByteArrayOutputStream baos = new ByteArrayOutputStream(2048);
+	private volatile BufferedReader reader;
+
+	private final Logger logger;
+
+	private final ByteArrayOutputStream cachedContent;
 
 	public LoggingBodyHttpServletRequest(HttpServletRequest request, Logger logger) {
 		super(request);
+		int contentLength = request.getContentLength();
+		this.cachedContent = new ByteArrayOutputStream(contentLength >= 0 ? contentLength : 1024);
 		this.logger = logger;
 	}
 
 	@Override
+	public String getCharacterEncoding() {
+		String enc = super.getCharacterEncoding();
+		return (enc != null ? enc : WebUtils.DEFAULT_CHARACTER_ENCODING);
+	}
+
+	@Override
 	public ServletInputStream getInputStream() throws IOException {
-		final ServletInputStream is = super.getInputStream();
 		ServletInputStream temp = servletInputStream;
 		if (temp == null) {
 			synchronized (this) {
 				temp = servletInputStream;
-				if (temp == null) {
-					servletInputStream = temp = new ServletInputStream() {
-
-						@Override
-						public int read() throws IOException {
-							int i = is.read();
-							if (i != -1)
-								baos.write(i);
-							return i;
-						}
-
-						@Override
-						public void setReadListener(ReadListener readListener) {
-							is.setReadListener(readListener);
-
-						}
-
-						@Override
-						public boolean isReady() {
-							return is.isReady();
-						}
-
-						@Override
-						public boolean isFinished() {
-							return is.isFinished();
-						}
-
-						@Override
-						public void close() throws IOException {
-							super.close();
-							byte[] bytes = baos.toByteArray();
-							baos.close();
-							baos = null;
-							String encoding = getCharacterEncoding();
-							if (encoding == null)
-								encoding = "UTF-8";
-							String str = new String(bytes, 0, bytes.length, encoding);
-							if (AppInfo.getStage() != Stage.DEVELOPMENT)
-								str = JsonDesensitizer.DEFAULT_INSTANCE.desensitize(str);
-							logger.info("\n{}", str);
-						}
-
-					};
-				}
+				if (temp == null)
+					servletInputStream = temp = new ContentCachingInputStream(super.getInputStream());
 			}
 		}
 		return temp;
+	}
+
+	@Override
+	public BufferedReader getReader() throws IOException {
+		if (this.reader == null) {
+			this.reader = new BufferedReader(new InputStreamReader(getInputStream(), getCharacterEncoding()));
+		}
+		return this.reader;
+	}
+
+	public byte[] getContentAsByteArray() {
+		return this.cachedContent.toByteArray();
+	}
+
+	private class ContentCachingInputStream extends ServletInputStream {
+
+		private final ServletInputStream is;
+
+		public ContentCachingInputStream(ServletInputStream is) {
+			this.is = is;
+		}
+
+		@Override
+		public int readLine(final byte[] b, final int off, final int len) throws IOException {
+			int count = is.readLine(b, off, len);
+			cache(b, off, count);
+			return count;
+		}
+
+		@Override
+		public int read(final byte[] b) throws IOException {
+			int count = super.read(b);
+			cachedContent.write(b, 0, count);
+			return count;
+		}
+
+		@Override
+		public int read(final byte[] b, final int off, final int len) throws IOException {
+			int count = is.read(b, off, len);
+			cache(b, off, count);
+			return count;
+		}
+
+		private void cache(final byte[] b, final int off, final int count) {
+			cachedContent.write(b, off, count);
+		}
+
+		@Override
+		public int read() throws IOException {
+			int ch = this.is.read();
+			if (ch != -1)
+				cachedContent.write(ch);
+			return ch;
+		}
+
+		@Override
+		public boolean isFinished() {
+			return this.is.isFinished();
+		}
+
+		@Override
+		public boolean isReady() {
+			return this.is.isReady();
+		}
+
+		@Override
+		public void setReadListener(ReadListener readListener) {
+			this.is.setReadListener(readListener);
+		}
+
+		@Override
+		public void close() throws IOException {
+			this.is.close();
+			byte[] bytes = getContentAsByteArray();
+			String str = new String(bytes, 0, bytes.length, getCharacterEncoding());
+			if (AppInfo.getStage() != Stage.DEVELOPMENT)
+				str = JsonDesensitizer.DEFAULT_INSTANCE.desensitize(str);
+			logger.info("\n{}", str);
+		}
 	}
 
 }
--- src/org/ironrhino/core/servlet/LoggingBodyHttpServletResponse.java
+++ src/org/ironrhino/core/servlet/LoggingBodyHttpServletResponse.java
@@ -1,7 +1,8 @@
 package org.ironrhino.core.servlet;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
@@ -13,14 +14,21 @@ import org.ironrhino.core.util.AppInfo.Stage;
 import org.ironrhino.core.util.JsonDesensitizer;
 import org.slf4j.Logger;
 import org.slf4j.MDC;
+import org.springframework.util.FastByteArrayOutputStream;
+import org.springframework.web.util.WebUtils;
 
+/**
+ * @See org.springframework.web.util.ContentCachingResponseWrapper
+ */
 public class LoggingBodyHttpServletResponse extends HttpServletResponseWrapper {
 
-	private Logger logger;
+	private final Logger logger;
 
 	private volatile ServletOutputStream streamOutputStream;
 
-	private ByteArrayOutputStream baos = new ByteArrayOutputStream(2048);
+	private PrintWriter writer;
+
+	private final FastByteArrayOutputStream content = new FastByteArrayOutputStream(1024);
 
 	public LoggingBodyHttpServletResponse(HttpServletResponse response, Logger logger) {
 		super(response);
@@ -28,62 +36,164 @@ public class LoggingBodyHttpServletResponse extends HttpServletResponseWrapper {
 	}
 
 	@Override
+	public String getCharacterEncoding() {
+		String enc = super.getCharacterEncoding();
+		return (enc != null ? enc : WebUtils.DEFAULT_CHARACTER_ENCODING);
+	}
+
+	@Override
+	public void setContentLength(int len) {
+		super.setContentLength(len);
+		if (len > this.content.size()) {
+			this.content.resize(len);
+		}
+	}
+
+	@Override
+	public void setContentLengthLong(long len) {
+		super.setContentLengthLong(len);
+		if (len > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Content-Length exceeds ContentCachingResponseWrapper's maximum ("
+					+ Integer.MAX_VALUE + "): " + len);
+		}
+		int lenInt = (int) len;
+		if (lenInt > this.content.size()) {
+			this.content.resize(lenInt);
+		}
+	}
+
+	@Override
+	public void setBufferSize(int size) {
+		super.setBufferSize(size);
+		if (size > this.content.size()) {
+			this.content.resize(size);
+		}
+	}
+
+	@Override
+	public void resetBuffer() {
+		super.resetBuffer();
+		this.content.reset();
+	}
+
+	@Override
+	public void reset() {
+		super.reset();
+		this.content.reset();
+	}
+
+	public byte[] getContentAsByteArray() {
+		return this.content.toByteArray();
+	}
+
+	@Override
 	public ServletOutputStream getOutputStream() throws IOException {
-		final ServletOutputStream os = super.getOutputStream();
 		ServletOutputStream temp = streamOutputStream;
 		if (temp == null) {
 			synchronized (this) {
 				temp = streamOutputStream;
-				if (temp == null) {
-					streamOutputStream = temp = new ServletOutputStream() {
-
-						@Override
-						public boolean isReady() {
-							return os.isReady();
-						}
-
-						@Override
-						public void setWriteListener(WriteListener writeListener) {
-							os.setWriteListener(writeListener);
-						}
-
-						@Override
-						public void write(int b) throws IOException {
-							os.write(b);
-							baos.write(b);
-						}
-
-						@Override
-						public void flush() throws IOException {
-							os.flush();
-						}
-
-						@Override
-						public void close() throws IOException {
-							os.flush();
-							os.close();
-							if (baos != null) {
-								byte[] bytes = baos.toByteArray();
-								baos.close();
-								baos = null;
-								String encoding = getCharacterEncoding();
-								if (encoding == null)
-									encoding = "UTF-8";
-								MDC.remove("method");
-								MDC.remove("url");
-								String str = new String(bytes, 0, bytes.length, encoding);
-								if (AppInfo.getStage() != Stage.DEVELOPMENT)
-									str = JsonDesensitizer.DEFAULT_INSTANCE.desensitize(str);
-								logger.info("\n{}", str);
-							}
-						}
-
-					};
-				}
+				if (temp == null)
+					streamOutputStream = temp = new ResponseServletOutputStream(super.getOutputStream());
 			}
 		}
 		return temp;
+	}
 
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		if (this.writer == null) {
+			this.writer = new ResponsePrintWriter(getCharacterEncoding());
+		}
+		return this.writer;
+	}
+
+	private class ResponseServletOutputStream extends ServletOutputStream {
+
+		private final ServletOutputStream os;
+
+		public ResponseServletOutputStream(ServletOutputStream os) {
+			this.os = os;
+		}
+
+		@Override
+		public void write(int b) throws IOException {
+			this.os.write(b);
+			content.write(b);
+		}
+
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
+			this.os.write(b, off, len);
+			content.write(b, off, len);
+		}
+
+		@Override
+		public void write(byte[] b) throws IOException {
+			this.os.write(b);
+			content.write(b);
+		}
+
+		@Override
+		public boolean isReady() {
+			return this.os.isReady();
+		}
+
+		@Override
+		public void setWriteListener(WriteListener writeListener) {
+			this.os.setWriteListener(writeListener);
+		}
+
+		@Override
+		public void close() throws IOException {
+			this.os.close();
+			byte[] bytes = getContentAsByteArray();
+			if (bytes.length > 0) {
+				String encoding = getCharacterEncoding();
+				MDC.remove("method");
+				MDC.remove("url");
+				String str = new String(bytes, 0, bytes.length, encoding);
+				if (AppInfo.getStage() != Stage.DEVELOPMENT)
+					str = JsonDesensitizer.DEFAULT_INSTANCE.desensitize(str);
+				logger.info("\n{}", str);
+			}
+		}
+	}
+
+	private class ResponsePrintWriter extends PrintWriter {
+
+		public ResponsePrintWriter(String characterEncoding) throws IOException {
+			super(new OutputStreamWriter(getOutputStream(), characterEncoding));
+		}
+
+		@Override
+		public void write(char[] buf, int off, int len) {
+			super.write(buf, off, len);
+			super.flush();
+		}
+
+		@Override
+		public void write(String s, int off, int len) {
+			super.write(s, off, len);
+			super.flush();
+		}
+
+		@Override
+		public void write(char[] buf) {
+			super.write(buf);
+			super.flush();
+		}
+
+		@Override
+		public void write(String s) {
+			super.write(s);
+			super.flush();
+		}
+
+		@Override
+		public void write(int c) {
+			super.write(c);
+			super.flush();
+		}
 	}
 
 }
