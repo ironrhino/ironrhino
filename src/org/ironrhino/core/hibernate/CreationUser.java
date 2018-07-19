6da7ffad4795a22ea9576c0b4caa3caacdf74c0f
Yanming Zhou <zhouyanming@gmail.com>
2018-07-19 12:08:19 +0800

    Add @target annotations

--- src/org/ironrhino/core/hibernate/CreationUser.java
+++ src/org/ironrhino/core/hibernate/CreationUser.java
@@ -1,11 +1,16 @@
 package org.ironrhino.core.hibernate;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.hibernate.annotations.ValueGenerationType;
 
 @ValueGenerationType(generatedBy = CreationUserGeneration.class)
+@Target({ FIELD, METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CreationUser {
 
