99f696b9dae8d2d11b7fdc7a176155d894bfe7b8
Yanming Zhou <zhouyanming@gmail.com>
2018-07-31 09:35:20 +0800

    Fix typo

--- src/org/ironrhino/core/spring/configuration/KafkaConsumerConfigBase.java
+++ src/org/ironrhino/core/spring/configuration/KafkaConsumerConfigBase.java
@@ -29,7 +29,7 @@ import lombok.Setter;
 @EnableKafka
 @Getter
 @Setter
-public class KafkaComsumerConfigBase {
+public class KafkaConsumerConfigBase {
 
 	@Value("${kafka.bootstrap.servers:localhost:9092}")
 	private String bootstrapServers;
--- src/org/ironrhino/sample/kafka/consumer/KafkaConsumerConfiguration.java
+++ src/org/ironrhino/sample/kafka/consumer/KafkaConsumerConfiguration.java
@@ -1,12 +1,12 @@
 package org.ironrhino.sample.kafka.consumer;
 
 import org.ironrhino.core.metadata.Profiles;
-import org.ironrhino.core.spring.configuration.KafkaComsumerConfigBase;
+import org.ironrhino.core.spring.configuration.KafkaConsumerConfigBase;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile(Profiles.SANDBOX)
 @Configuration
-public class KafkaComsumerConfiguration extends KafkaComsumerConfigBase {
+public class KafkaConsumerConfiguration extends KafkaConsumerConfigBase {
 
 }
