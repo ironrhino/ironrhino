92c969581bcac475f9c0a8844d628c5c5fd8baf6
Yanming Zhou <zhouyanming@gmail.com>
2018-08-03 17:50:13 +0800

    Add sneakyThrow method
    private static void throwsSneakyIOException() {
        sneakyThrow(new IOException("sneaky"));
    }

--- src/org/ironrhino/core/util/ExceptionUtils.java
+++ src/org/ironrhino/core/util/ExceptionUtils.java
@@ -65,4 +65,10 @@ public abstract class ExceptionUtils {
 			ex.setStackTrace(combinedStack);
 		}
 	}
+
+	@SuppressWarnings("unchecked")
+	public static <E extends Throwable> void sneakyThrow(Throwable e) throws E {
+		throw (E) e;
+	}
+
 }
