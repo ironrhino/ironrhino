80d431ec5f865efaa285dbfe0be8b86f838a131b
Yanming Zhou <zhouyanming@gmail.com>
2018-09-11 16:48:39 +0800

    Improve session compressor

--- src/org/ironrhino/core/session/impl/DefaultSessionCompressor.java
+++ src/org/ironrhino/core/session/impl/DefaultSessionCompressor.java
@@ -1,12 +1,19 @@
 package org.ironrhino.core.session.impl;
 
 import org.ironrhino.core.session.SessionCompressor;
-import org.ironrhino.core.util.JsonUtils;
-import org.ironrhino.core.util.ReflectionUtils;
+import org.ironrhino.core.util.JsonSerializationUtils;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class DefaultSessionCompressor implements SessionCompressor<Object> {
 
-	private static final String SEPERATOR = "@";
+	private final ObjectMapper objectMapper;
+
+	public DefaultSessionCompressor() {
+		objectMapper = JsonSerializationUtils.createNewObjectMapper()
+				.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+	}
 
 	@Override
 	public boolean supportsKey(String key) {
@@ -17,25 +24,24 @@ public class DefaultSessionCompressor implements SessionCompressor<Object> {
 	public String compress(Object value) throws Exception {
 		if (value == null)
 			return null;
-		if (value instanceof String)
-			return (String) value;
-		return ReflectionUtils.getActualClass(value).getName() + SEPERATOR + JsonUtils.toJson(value);
+		if (value instanceof Long)
+			return value + "L";
+		else if (value instanceof Float)
+			return value + "F";
+		return objectMapper.writeValueAsString(value);
 	}
 
 	@Override
 	public Object uncompress(String string) throws Exception {
-		if (string == null)
-			return null;
-		int index = string.indexOf(SEPERATOR);
-		if (index < 0)
+		if (string == null || string.isEmpty())
 			return string;
-		try {
-			String className = string.substring(0, index);
-			String json = string.substring(index + SEPERATOR.length());
-			return JsonUtils.fromJson(json, Class.forName(className));
-		} catch (ClassNotFoundException e) {
-			return string;
+		if (Character.isDigit(string.charAt(0))) {
+			if (string.endsWith("L"))
+				return Long.valueOf(string.substring(0, string.length() - 1));
+			else if (string.endsWith("F"))
+				return Float.valueOf(string.substring(0, string.length() - 1));
 		}
+		return objectMapper.readValue(string, Object.class);
 	}
 
 }
