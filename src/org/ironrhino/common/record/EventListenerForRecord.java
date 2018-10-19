1a2dfb0dab93ad62113186b1dad0032dd7a7bed9
Yanming Zhou <zhouyanming@gmail.com>
2018-10-19 11:39:45 +0800

    Use post commit event listener

--- src/org/ironrhino/common/record/EventListenerForRecord.java
+++ src/org/ironrhino/common/record/EventListenerForRecord.java
@@ -1,18 +1,18 @@
 package org.ironrhino.common.record;
 
+import org.hibernate.event.spi.PostCommitDeleteEventListener;
+import org.hibernate.event.spi.PostCommitInsertEventListener;
+import org.hibernate.event.spi.PostCommitUpdateEventListener;
 import org.hibernate.event.spi.PostDeleteEvent;
-import org.hibernate.event.spi.PostDeleteEventListener;
 import org.hibernate.event.spi.PostInsertEvent;
-import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.event.spi.PostUpdateEvent;
-import org.hibernate.event.spi.PostUpdateEventListener;
 import org.hibernate.persister.entity.EntityPersister;
 import org.ironrhino.core.util.ReflectionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventListenerForRecord
-		implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
+		implements PostCommitInsertEventListener, PostCommitUpdateEventListener, PostCommitDeleteEventListener {
 
 	private static final long serialVersionUID = 6292673725187749565L;
 
@@ -35,6 +35,21 @@ public class EventListenerForRecord
 	}
 
 	@Override
+	public void onPostDeleteCommitFailed(PostDeleteEvent event) {
+
+	}
+
+	@Override
+	public void onPostUpdateCommitFailed(PostUpdateEvent event) {
+
+	}
+
+	@Override
+	public void onPostInsertCommitFailed(PostInsertEvent event) {
+
+	}
+
+	@Override
 	public boolean requiresPostCommitHanding(EntityPersister persister) {
 		return false;
 	}
@@ -42,4 +57,5 @@ public class EventListenerForRecord
 	private static boolean isAnnotated(Object entity) {
 		return ReflectionUtils.getActualClass(entity).isAnnotationPresent(RecordAware.class);
 	}
+
 }
--- src/org/ironrhino/core/aop/EventListenerForPublish.java
+++ src/org/ironrhino/core/aop/EventListenerForPublish.java
@@ -1,18 +1,18 @@
 package org.ironrhino.core.aop;
 
+import org.hibernate.event.spi.PostCommitDeleteEventListener;
+import org.hibernate.event.spi.PostCommitInsertEventListener;
+import org.hibernate.event.spi.PostCommitUpdateEventListener;
 import org.hibernate.event.spi.PostDeleteEvent;
-import org.hibernate.event.spi.PostDeleteEventListener;
 import org.hibernate.event.spi.PostInsertEvent;
-import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.event.spi.PostUpdateEvent;
-import org.hibernate.event.spi.PostUpdateEventListener;
 import org.hibernate.persister.entity.EntityPersister;
 import org.ironrhino.core.util.ReflectionUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventListenerForPublish
-		implements PostInsertEventListener, PostUpdateEventListener, PostDeleteEventListener {
+		implements PostCommitInsertEventListener, PostCommitUpdateEventListener, PostCommitDeleteEventListener {
 
 	private static final long serialVersionUID = 9062685218966998574L;
 
@@ -32,6 +32,21 @@ public class EventListenerForPublish
 	public void onPostUpdate(PostUpdateEvent event) {
 		if (isAnnotated(event.getEntity()))
 			PublishAspect.getHibernateEvents(true).add(event);
+	}
+
+	@Override
+	public void onPostDeleteCommitFailed(PostDeleteEvent event) {
+
+	}
+
+	@Override
+	public void onPostUpdateCommitFailed(PostUpdateEvent event) {
+
+	}
+
+	@Override
+	public void onPostInsertCommitFailed(PostInsertEvent event) {
+
 	}
 
 	@Override
