1a7dfc16d78373f3d3d3d696b47ea040b9dec136
Yanming Zhou <zhouyanming@gmail.com>
2018-12-07 09:01:31 +0800

    Improve hashCode and equals and toString for AbstractEntity
    https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/

--- src/org/ironrhino/core/model/AbstractEntity.java
+++ src/org/ironrhino/core/model/AbstractEntity.java
@@ -14,14 +14,14 @@ public abstract class AbstractEntity<PK extends Serializable> implements Persist
 	@Override
 	public int hashCode() {
 		HashCodeBuilder builder = new HashCodeBuilder();
-		Map<String, Object> map = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
-		if (!map.isEmpty()) {
-			for (Object value : map.values())
+		Map<String, Object> naturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
+		if (!naturalIds.isEmpty()) {
+			for (Object value : naturalIds.values())
 				builder.append(value);
+			return builder.toHashCode();
 		} else {
-			builder.append(this.getId());
+			return 31;
 		}
-		return builder.toHashCode();
 	}
 
 	@Override
@@ -34,39 +34,32 @@ public abstract class AbstractEntity<PK extends Serializable> implements Persist
 		if (!this.getClass().isAssignableFrom(object.getClass())
 				&& !object.getClass().isAssignableFrom(this.getClass()))
 			return false;
-		AbstractEntity that = (AbstractEntity) object;
-		if (this.getId() != null)
-			return this.getId().equals(that.getId());
-		if (that.getId() != null)
-			return that.getId().equals(this.getId());
-		Map<String, Object> thisNaturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
-		if (!thisNaturalIds.isEmpty()) {
-			Map<String, Object> thatNaturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(that,
+		Map<String, Object> naturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
+		if (!naturalIds.isEmpty()) {
+			Map<String, Object> thatNaturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(object,
 					NaturalId.class);
-			for (Map.Entry<String, Object> entry : thisNaturalIds.entrySet()) {
+			for (Map.Entry<String, Object> entry : naturalIds.entrySet()) {
 				Object thisValue = entry.getValue();
 				Object thatValue = thatNaturalIds.get(entry.getKey());
 				if (thisValue == null || thatValue == null || !thisValue.equals(thatValue))
 					return false;
 			}
 			return true;
+		} else {
+			return this.getId() != null && this.getId().equals(((AbstractEntity) object).getId());
 		}
-		return false;
 	}
 
 	@Override
 	public String toString() {
-		Map<String, Object> map = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
-		if (map.size() == 1) {
-			Object naturalId = map.values().iterator().next();
-			if (naturalId != null)
-				return String.valueOf(naturalId);
-		} else if (map.size() > 1) {
-			for (Object v : map.values())
-				if (v != null)
-					return map.toString();
+		Map<String, Object> naturalIds = AnnotationUtils.getAnnotatedPropertyNameAndValues(this, NaturalId.class);
+		if (naturalIds.size() == 1) {
+			return String.valueOf(naturalIds.values().iterator().next());
+		} else if (naturalIds.size() > 1) {
+			return getClass().getSimpleName() + '@' + String.valueOf(naturalIds);
+		} else {
+			return getClass().getSimpleName() + '@' + String.valueOf(getId());
 		}
-		return String.valueOf(getId());
 	}
 
 }
