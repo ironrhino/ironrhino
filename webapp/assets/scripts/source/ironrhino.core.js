914ed995fb3762c760bfcdd02e8387855207fcd6
Yanming Zhou <zhouyanming@gmail.com>
2018-12-11 12:48:58 +0800

    Success should without actionErrors or fieldErrors

--- webapp/assets/scripts/source/ironrhino.core.js
+++ webapp/assets/scripts/source/ironrhino.core.js
@@ -1494,27 +1494,42 @@ Observation.common = function(container) {
 					Ajax.fire(target, 'onerror');
 				},
 				success : function(data, textStatus, xhr) {
-					$('.remove-on-success', target).remove();
-					$(':input.readonly-on-success', target).prop('readonly',
-							true);
-					$(':input.disabled-on-success', target).prop('disabled',
-							true);
-					var postback = xhr.getResponseHeader('X-Postback');
-					if (postback)
-						$.each(postback.split(', '), function(i, v) {
-									var pair = v.split('=', 2);
-									var input = $('[name="' + pair[0] + '"]',
-											target);
-									if (!input.length) {
-										var con = $('fieldset', target);
-										if (!con.length)
-											con = $(target);
-										input = $('<input type="hidden" name="'
-												+ pair[0] + '">')
-												.prependTo(con);
+					if (!data || !(data.fieldErrors || data.actionErrors)) {
+						$('.remove-on-success', target).remove();
+						$(':input.readonly-on-success', target).each(
+								function() {
+									var t = $(this);
+									if (t.is('select')) {
+										t
+												.prop('disabled', true)
+												.after('<input type="hidden" name="'
+														+ t.attr('name')
+														+ '" value="'
+														+ t.val()
+														+ '">');
+									} else {
+										t.prop('readonly', true);
 									}
-									input.val(pair[1]);
 								});
+						$(':input.disabled-on-success', target).prop(
+								'disabled', true);
+						var postback = xhr.getResponseHeader('X-Postback');
+						if (postback)
+							$.each(postback.split(', '), function(i, v) {
+										var pair = v.split('=', 2);
+										var input = $('[name="' + pair[0]
+														+ '"]', target);
+										if (!input.length) {
+											var con = $('fieldset', target);
+											if (!con.length)
+												con = $(target);
+											input = $('<input type="hidden" name="'
+													+ pair[0] + '">')
+													.prependTo(con);
+										}
+										input.val(pair[1]);
+									});
+					}
 					Ajax.handleResponse(data, _opt, xhr);
 				},
 				complete : function() {
