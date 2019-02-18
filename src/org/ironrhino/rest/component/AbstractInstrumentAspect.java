e3000aa0925a80727062569105e679f46ee5482a
Yanming Zhou <zhouyanming@gmail.com>
2019-02-18 15:51:25 +0800

    Extract AbstractInstrumentAspect from MetricsAspect

---
+++ src/org/ironrhino/rest/component/AbstractInstrumentAspect.java
@@ -0,0 +1,47 @@
+package org.ironrhino.rest.component;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.ironrhino.core.aop.BaseAspect;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
+
+abstract class AbstractInstrumentAspect extends BaseAspect {
+
+	protected final String servletMapping;
+
+	public AbstractInstrumentAspect(String servletMapping) {
+		if (servletMapping.endsWith("/*"))
+			servletMapping = servletMapping.substring(0, servletMapping.length() - 2);
+		this.servletMapping = servletMapping;
+	}
+
+	protected Mapping getMapping(ProceedingJoinPoint pjp) {
+		RequestMapping requestMapping = AnnotatedElementUtils
+				.findMergedAnnotation(((MethodSignature) pjp.getSignature()).getMethod(), RequestMapping.class);
+		if (requestMapping == null)
+			return null;
+		RequestMapping requestMappingWithClass = AnnotatedElementUtils.findMergedAnnotation(pjp.getTarget().getClass(),
+				RequestMapping.class);
+		String method = requestMapping.method().length > 0 ? requestMapping.method()[0].toString() : "GET";
+		StringBuilder sb = new StringBuilder(servletMapping);
+		if (requestMappingWithClass != null) {
+			String[] pathWithClass = requestMappingWithClass.path();
+			if (pathWithClass.length > 0)
+				sb.append(pathWithClass[0]);
+		}
+		String[] path = requestMapping.path();
+		sb.append(path.length > 0 ? path[0] : "");
+		String uri = sb.toString();
+		return new Mapping(method, uri);
+	}
+
+	@Data
+	static class Mapping {
+		private final String method;
+		private final String uri;
+	}
+
+}
--- src/org/ironrhino/rest/component/MetricsAspect.java
+++ src/org/ironrhino/rest/component/MetricsAspect.java
@@ -3,46 +3,29 @@ package org.ironrhino.rest.component;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.ironrhino.core.aop.BaseAspect;
 import org.ironrhino.core.metrics.Metrics;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Aspect
 @ControllerAdvice
-public class MetricsAspect extends BaseAspect {
-
-	private final String servletMapping;
+public class MetricsAspect extends AbstractInstrumentAspect {
 
 	public MetricsAspect(String servletMapping) {
-		if (servletMapping.endsWith("/*"))
-			servletMapping = servletMapping.substring(0, servletMapping.length() - 2);
-		this.servletMapping = servletMapping;
+		super(servletMapping);
 		order = Ordered.HIGHEST_PRECEDENCE + 2;
 	}
 
 	@Around("execution(public * *(..)) and @within(restController)")
 	public Object timing(ProceedingJoinPoint pjp, RestController restController) throws Throwable {
-		RequestMapping requestMapping = AnnotatedElementUtils
-				.findMergedAnnotation(((MethodSignature) pjp.getSignature()).getMethod(), RequestMapping.class);
-		if (requestMapping == null)
+		if (Metrics.isMicrometerPresent())
 			return pjp.proceed();
-		RequestMapping requestMappingWithClass = AnnotatedElementUtils.findMergedAnnotation(pjp.getTarget().getClass(),
-				RequestMapping.class);
-		StringBuilder sb = new StringBuilder(servletMapping);
-		if (requestMappingWithClass != null) {
-			String[] pathWithClass = requestMappingWithClass.path();
-			if (pathWithClass.length > 0)
-				sb.append(pathWithClass[0]);
-		}
-		String[] path = requestMapping.path();
-		sb.append(path.length > 0 ? path[0] : "");
-		String method = requestMapping.method().length > 0 ? requestMapping.method()[0].toString() : "GET";
-		return Metrics.recordThrowableCallable("rest.calls", pjp::proceed, "uri", sb.toString(), "method", method);
+		Mapping mapping = getMapping(pjp);
+		if (mapping == null)
+			return pjp.proceed();
+		return Metrics.recordThrowableCallable("rest.calls", pjp::proceed, "method", mapping.getMethod(), "uri",
+				mapping.getUri());
 	}
 
 }
