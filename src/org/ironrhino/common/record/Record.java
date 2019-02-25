637eab7a801b74509f2317dce9b9ff42e61519db
Yanming Zhou <zhouyanming@gmail.com>
2019-02-25 11:03:39 +0800

    Polishing

--- src/org/ironrhino/common/record/Record.java
+++ src/org/ironrhino/common/record/Record.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import org.hibernate.criterion.MatchMode;
 import org.ironrhino.core.metadata.AppendOnly;
 import org.ironrhino.core.metadata.AutoConfig;
 import org.ironrhino.core.metadata.Readonly;
@@ -20,27 +21,27 @@ import lombok.Setter;
 @AppendOnly
 @Entity
 @Table(name = "common_record")
-@Richtable(order = "recordDate desc", readonly = @Readonly(value = true))
+@Richtable(order = "recordDate desc", readonly = @Readonly(value = true), showQueryForm = true)
 @Getter
 @Setter
 public class Record extends BaseEntity {
 
 	private static final long serialVersionUID = -8287907984213799302L;
 
-	@UiConfig(width = "300px")
+	@UiConfig(width = "300px", queryMatchMode = MatchMode.EXACT)
 	private String entityClass;
 
-	@UiConfig(width = "200px")
+	@UiConfig(width = "200px", queryMatchMode = MatchMode.EXACT)
 	private String entityId;
 
 	@UiConfig
 	@Column(length = 10000)
 	private String entityToString;
 
-	@UiConfig(width = "80px")
+	@UiConfig(width = "80px", queryMatchMode = MatchMode.EXACT)
 	private String action;
 
-	@UiConfig(width = "100px")
+	@UiConfig(width = "100px", queryMatchMode = MatchMode.EXACT)
 	private String operatorId;
 
 	@UiConfig(width = "130px")
