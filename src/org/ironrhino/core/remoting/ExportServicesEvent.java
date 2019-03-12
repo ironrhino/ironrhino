62c19dd2e0041eb064397f2ff0777d9ac96fad62
Yanming Zhou <zhouyanming@gmail.com>
2019-03-12 10:41:12 +0800

    Fix potential NPE

--- src/org/ironrhino/core/remoting/ExportServicesEvent.java
+++ src/org/ironrhino/core/remoting/ExportServicesEvent.java
@@ -8,18 +8,21 @@ import org.ironrhino.core.event.InstanceLifecycleEvent;
 
 import lombok.Getter;
 
-@Getter
 public class ExportServicesEvent extends InstanceLifecycleEvent {
 
 	private static final long serialVersionUID = 4564138152726138645L;
 
+	@Getter
 	private List<String> exportServices;
 
 	private Map<String, String> servicePaths;
 
+	public Map<String, String> getServicePaths() {
+		return servicePaths != null ? servicePaths : Collections.emptyMap();
+	}
+
 	public ExportServicesEvent(List<String> exportServices) {
 		this.exportServices = exportServices;
-		this.servicePaths = Collections.emptyMap();
 	}
 
 	public ExportServicesEvent(List<String> exportServices, Map<String, String> servicePaths) {
