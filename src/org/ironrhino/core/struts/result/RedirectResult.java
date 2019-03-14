80111b96ca3fd86d8acca8fe7b76b003988cf093
Yanming Zhou <zhouyanming@gmail.com>
2019-03-14 08:20:56 +0800

    Revert "Polishing"
    This reverts commit 19454253e23aeb5f523b5e1b0987ac0a58837194.

--- src/org/ironrhino/core/struts/result/RedirectResult.java
+++ src/org/ironrhino/core/struts/result/RedirectResult.java
@@ -3,6 +3,7 @@ package org.ironrhino.core.struts.result;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.dispatcher.StrutsResultSupport;
 import org.ironrhino.core.util.RequestUtils;
@@ -30,9 +31,17 @@ public class RedirectResult extends StrutsResultSupport {
 		HttpServletRequest request = ServletActionContext.getRequest();
 		HttpServletResponse response = ServletActionContext.getResponse();
 		if (finalLocation.indexOf("://") < 0) {
-			if (finalLocation.startsWith("/"))
-				finalLocation = request.getContextPath() + finalLocation;
-			finalLocation = response.encodeRedirectURL(finalLocation);
+			String url = request.getRequestURL().toString();
+			if (finalLocation.startsWith("/")) {
+				String ctxPath = request.getContextPath();
+				finalLocation = url.substring(0,
+						url.indexOf(StringUtils.isBlank(ctxPath) ? "/" : ctxPath, url.indexOf("://") + 3)
+								+ ctxPath.length())
+						+ finalLocation;
+			} else {
+				finalLocation = url.substring(0, url.lastIndexOf('/') + 1) + finalLocation;
+			}
+			finalLocation = response.encodeRedirectURL(finalLocation.toString());
 		} else if (!RequestUtils.isSameOrigin(request, finalLocation)) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN);
 		}
