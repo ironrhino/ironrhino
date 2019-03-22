29cc7af85be35fddf4965e7eecc6c21638dd7de4
Yanming Zhou <zhouyanming@gmail.com>
2019-03-22 15:08:41 +0800

    Polishing

--- src/org/ironrhino/core/event/ApplicationEventMulticaster.java
+++ src/org/ironrhino/core/event/ApplicationEventMulticaster.java
@@ -1,22 +1,19 @@
 package org.ironrhino.core.event;
 
-import java.util.concurrent.ExecutorService;
-
 import javax.annotation.PostConstruct;
 
 import org.ironrhino.core.spring.configuration.ApplicationContextPropertiesConditional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Component;
 
 @ApplicationContextPropertiesConditional(key = "applicationEventMulticaster.async", value = "true")
 @Component
 public class ApplicationEventMulticaster extends SimpleApplicationEventMulticaster {
 
-	@Autowired(required = false)
-	@Qualifier("executorService")
-	private ExecutorService taskExecutor;
+	@Autowired
+	private AsyncTaskExecutor taskExecutor;
 
 	@PostConstruct
 	public void afterPropertiesSet() {
