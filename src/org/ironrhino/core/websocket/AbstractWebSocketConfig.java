001ce1e12b1e2fdcd4560dbd4a8a080620fbaf5c
Yanming Zhou <zhouyanming@gmail.com>
2019-05-29 09:13:00 +0800

    Allow websocket open for all origin

--- src/org/ironrhino/core/websocket/AbstractWebSocketConfig.java
+++ src/org/ironrhino/core/websocket/AbstractWebSocketConfig.java
@@ -4,13 +4,18 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @EnableWebSocket
 public abstract class AbstractWebSocketConfig implements WebSocketConfigurer {
+
+	@Value("${cors.openForAllOrigin:false}")
+	private boolean openForAllOrigin;
 
 	@Autowired
 	private Map<String, WebSocketHandler> webSocketHandlers = Collections.emptyMap();
@@ -25,7 +30,9 @@ public abstract class AbstractWebSocketConfig implements WebSocketConfigurer {
 				name = name.substring(0, name.length() - "WebSocket".length());
 			else if (name.endsWith("Handler"))
 				name = name.substring(0, name.length() - "Handler".length());
-			registry.addHandler(entry.getValue(), name);
+			WebSocketHandlerRegistration registration = registry.addHandler(entry.getValue(), name);
+			if (openForAllOrigin)
+				registration.setAllowedOrigins("*");
 		});
 	}
 
