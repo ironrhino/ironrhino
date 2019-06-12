4f41254cd3a7c9300526c094262124b4e6b9f6b4
Yanming Zhou <zhouyanming@gmail.com>
2019-06-12 10:55:19 +0800

    Fix role occasional missing

--- src/org/ironrhino/core/remoting/client/HttpInvokerClient.java
+++ src/org/ironrhino/core/remoting/client/HttpInvokerClient.java
@@ -247,7 +247,6 @@ public class HttpInvokerClient extends FallbackSupportMethodInterceptorFactoryBe
 				MDC.remove(AccessFilter.MDC_KEY_REQUEST_ID);
 				MDC.remove("request");
 			}
-			MDC.remove("role");
 		}
 		return result;
 	}
