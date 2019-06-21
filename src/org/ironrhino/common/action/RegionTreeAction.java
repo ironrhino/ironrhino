f85e2ff5f49db9b6aaa5bd555d0662a916757082
Yanming Zhou <zhouyanming@gmail.com>
2019-06-21 18:15:47 +0800

    Make RegionTreeControl optional

--- src/org/ironrhino/common/action/RegionTreeAction.java
+++ src/org/ironrhino/common/action/RegionTreeAction.java
@@ -3,13 +3,16 @@ package org.ironrhino.common.action;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.ironrhino.common.model.Region;
 import org.ironrhino.core.hibernate.CriterionUtils;
 import org.ironrhino.core.metadata.AutoConfig;
 import org.ironrhino.core.metadata.JsonConfig;
 import org.ironrhino.core.service.BaseTreeControl;
+import org.ironrhino.core.service.EntityManager;
 import org.ironrhino.core.struts.BaseAction;
-import org.ironrhino.core.util.HtmlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.Getter;
@@ -36,21 +39,45 @@ public class RegionTreeAction extends BaseAction {
 	private Long parent;
 
 	@Autowired
+	private EntityManager<Region> entityManager;
+
+	@Autowired(required = false)
 	private BaseTreeControl<Region> regionTreeControl;
 
 	@JsonConfig(root = "children")
 	public String children() {
+		entityManager.setEntityClass(Region.class);
 		Region region;
 		if (parent == null || parent < 1) {
 			if (tree != null && tree > 0) {
 				children = new ArrayList<>();
-				children.add(regionTreeControl.getTree().getDescendantOrSelfById(tree));
+				if (regionTreeControl != null) {
+					children.add(regionTreeControl.getTree().getDescendantOrSelfById(tree));
+				} else {
+					region = new Region();
+					DetachedCriteria dc = entityManager.detachedCriteria();
+					dc.add(Restrictions.eq("id", tree));
+					region.setChildren(entityManager.findListByCriteria(dc));
+				}
 				return JSON;
 			} else {
-				region = regionTreeControl.getTree();
+				if (regionTreeControl != null) {
+					region = regionTreeControl.getTree();
+				} else {
+					region = new Region();
+					DetachedCriteria dc = entityManager.detachedCriteria();
+					dc.add(Restrictions.isNull("parent"));
+					dc.addOrder(Order.asc("displayOrder"));
+					dc.addOrder(Order.asc("name"));
+					region.setChildren(entityManager.findListByCriteria(dc));
+				}
 			}
 		} else {
-			region = regionTreeControl.getTree().getDescendantOrSelfById(parent);
+			if (regionTreeControl != null) {
+				region = regionTreeControl.getTree().getDescendantOrSelfById(parent);
+			} else {
+				region = entityManager.get(parent);
+			}
 		}
 		if (region != null)
 			children = region.getChildren();
@@ -58,30 +85,24 @@ public class RegionTreeAction extends BaseAction {
 		return JSON;
 	}
 
-	@Override
-	public String execute() {
-		if (!async) {
-			Region region;
-			if (parent == null || parent < 1)
-				region = regionTreeControl.getTree();
-			else
-				region = regionTreeControl.getTree().getDescendantOrSelfById(parent);
-			children = region.getChildren();
-		}
-		children = CriterionUtils.filter(children);
-		return SUCCESS;
-	}
-
 	public String table() {
 		return "table";
 	}
 
 	public Region getRegionTree() {
-		return regionTreeControl.getTree();
-	}
-
-	public String getTreeViewHtml() {
-		return HtmlUtils.getTreeViewHtml(children, async);
+		entityManager.setEntityClass(Region.class);
+		Region region;
+		if (regionTreeControl != null) {
+			region = regionTreeControl.getTree();
+		} else {
+			region = new Region();
+			DetachedCriteria dc = entityManager.detachedCriteria();
+			dc.add(Restrictions.isNull("parent"));
+			dc.addOrder(Order.asc("displayOrder"));
+			dc.addOrder(Order.asc("name"));
+			region.setChildren(entityManager.findListByCriteria(dc));
+		}
+		return region;
 	}
 
 }
--- src/org/ironrhino/common/support/RegionTreeControl.java
+++ src/org/ironrhino/common/support/RegionTreeControl.java
@@ -2,8 +2,10 @@ package org.ironrhino.common.support;
 
 import org.ironrhino.common.model.Region;
 import org.ironrhino.core.service.BaseTreeControl;
+import org.ironrhino.core.spring.configuration.ApplicationContextPropertiesConditional;
 import org.springframework.stereotype.Component;
 
+@ApplicationContextPropertiesConditional(key = "regionTreeControl.disabled", value = "true", negated = true)
 @Component
 public class RegionTreeControl extends BaseTreeControl<Region> {
 
