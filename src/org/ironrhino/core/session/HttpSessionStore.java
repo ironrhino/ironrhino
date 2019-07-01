a2ec7704868f35b0658b71ab20718332a32f4afa
Yanming Zhou <zhouyanming@gmail.com>
2019-07-01 14:02:12 +0800

    Fix typo

--- src/org/ironrhino/core/session/HttpSessionStore.java
+++ src/org/ironrhino/core/session/HttpSessionStore.java
@@ -2,10 +2,10 @@ package org.ironrhino.core.session;
 
 public interface HttpSessionStore {
 
-	public void save(WrappedHttpSession sesion);
+	public void save(WrappedHttpSession session);
 
 	public void initialize(WrappedHttpSession session);
 
-	public void invalidate(WrappedHttpSession sesion);
+	public void invalidate(WrappedHttpSession session);
 
 }
