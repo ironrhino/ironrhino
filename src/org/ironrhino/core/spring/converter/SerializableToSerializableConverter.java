e6b41dc6001844f1b77aaaf3c03cf0b3a15d9e58
Yanming Zhou <zhouyanming@gmail.com>
2019-07-08 17:24:00 +0800

    Only call public empty constructor

--- src/org/ironrhino/core/spring/converter/SerializableToSerializableConverter.java
+++ src/org/ironrhino/core/spring/converter/SerializableToSerializableConverter.java
@@ -67,7 +67,7 @@ public class SerializableToSerializableConverter implements ConditionalGenericCo
 			return source;
 		if (Persistable.class.isAssignableFrom(targetClass) && sourceClass == String.class) {
 			try {
-				Object target = org.springframework.beans.BeanUtils.instantiateClass(targetClass);
+				Object target = targetClass.getConstructor().newInstance();
 				BeanUtils.setPropertyValue(target, "id", source);
 				return target;
 			} catch (Exception e) {
@@ -85,7 +85,7 @@ public class SerializableToSerializableConverter implements ConditionalGenericCo
 			return bw.getPropertyValue("id");
 		}
 		try {
-			Object target = org.springframework.beans.BeanUtils.instantiateClass(targetClass);
+			Object target = targetClass.getConstructor().newInstance();
 			BeanUtils.copyProperties(source, target);
 			return target;
 		} catch (Exception e) {
