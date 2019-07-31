7ef45d10eb2ac68cec6f45aac5dffc0f76ba8072
Yanming Zhou <zhouyanming@gmail.com>
2019-07-31 16:07:06 +0800

    Make verification code predicable if stage is development

--- src/org/ironrhino/core/security/verfication/impl/DefaultVerificationCodeGenerator.java
+++ src/org/ironrhino/core/security/verfication/impl/DefaultVerificationCodeGenerator.java
@@ -1,7 +1,10 @@
 package org.ironrhino.core.security.verfication.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ironrhino.core.security.verfication.VerificationCodeGenerator;
 import org.ironrhino.core.spring.configuration.ApplicationContextPropertiesConditional;
+import org.ironrhino.core.util.AppInfo;
+import org.ironrhino.core.util.AppInfo.Stage;
 import org.ironrhino.core.util.CodecUtils;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,8 @@ public class DefaultVerificationCodeGenerator implements VerificationCodeGenerat
 
 	@Override
 	public String generator(String receiver, int length) {
+		if (AppInfo.getStage() == Stage.DEVELOPMENT)
+			return StringUtils.repeat('0', length);
 		return CodecUtils.randomDigitalString(length);
 	}
 
