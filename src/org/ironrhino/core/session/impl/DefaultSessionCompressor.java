a50f6c8b7b059b01c9879608635ee6548d173c8e
Yanming Zhou <zhouyanming@gmail.com>
2019-08-08 17:20:21 +0800

    Skip compress SavedRequest to avoid exception when uncompress
    in case of <security:request-cache ref="requestCache"/> not added

--- src/org/ironrhino/core/session/impl/DefaultSessionCompressor.java
+++ src/org/ironrhino/core/session/impl/DefaultSessionCompressor.java
@@ -2,6 +2,7 @@ package org.ironrhino.core.session.impl;
 
 import org.ironrhino.core.session.SessionCompressor;
 import org.ironrhino.core.util.JsonSerializationUtils;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 public class DefaultSessionCompressor implements SessionCompressor<Object> {
 
@@ -12,6 +13,8 @@ public class DefaultSessionCompressor implements SessionCompressor<Object> {
 
 	@Override
 	public String compress(Object object) throws Exception {
+		if (object instanceof SavedRequest)
+			return null;
 		return JsonSerializationUtils.serialize(object);
 	}
 
