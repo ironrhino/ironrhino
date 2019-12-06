7b49e3e9a60048fe8dc63e7b7dbe8d06c5561d30
Yanming Zhou <zhouyanming@gmail.com>
2019-12-06 08:56:02 +0800

    Polishing

--- src/org/ironrhino/sample/api/controller/ArticleService.java
+++ src/org/ironrhino/sample/api/controller/ArticleService.java
@@ -16,12 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
 @RestApi
-@RestController
 @RequestMapping("/article")
 @Authorize(ifAnyGranted = UserRole.ROLE_ADMINISTRATOR)
 @ApiModule(value = "文章API")
