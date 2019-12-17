81b78ba6178cba9099da1bc263c4f62aef120499
Yanming Zhou <zhouyanming@gmail.com>
2019-12-17 15:13:22 +0800

    Polishing

--- src/org/ironrhino/batch/DefaultJobStepExecutor.java
+++ src/org/ironrhino/batch/DefaultJobStepExecutor.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.Map;
 
 import org.ironrhino.core.util.ReflectionUtils;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInterruptedException;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
@@ -34,17 +33,16 @@ public class DefaultJobStepExecutor implements JobStepExecutor {
 	@Override
 	public void execute(Long jobExecutionId, Long stepExecutionId, String stepName)
 			throws JobInterruptedException, NoSuchJobException {
-		JobExecution jobExecution = jobExplorer.getJobExecution(jobExecutionId);
-		if (jobExecution == null)
-			throw new IllegalArgumentException("No such jobExecutionId: " + jobExecutionId);
-		String jobName = jobExecution.getJobInstance().getJobName();
+		StepExecution stepExecution = jobExplorer.getStepExecution(jobExecutionId, stepExecutionId);
+		if (stepExecution == null)
+			throw new IllegalArgumentException("No such StepExecution: " + stepExecutionId);
+		String jobName = stepExecution.getJobExecution().getJobInstance().getJobName();
 		log.info("Prepare execute step[{}#{}] of job[{}#{}]", stepName, stepExecutionId, jobName, jobExecutionId);
 		Map<ConfigurableApplicationContext, Collection<String>> contextToJobNames = ReflectionUtils
 				.getFieldValue(jobLoader, "contextToJobNames");
 		Step step = contextToJobNames.entrySet().stream().filter(entry -> entry.getValue().contains(jobName))
 				.map(entry -> entry.getKey()).findAny().map(ctx -> ctx.getBean(stepName, Step.class))
 				.orElseGet(() -> rootApplicationContext.getBean(stepName, Step.class));
-		StepExecution stepExecution = jobExplorer.getStepExecution(jobExecutionId, stepExecutionId);
 		step.execute(stepExecution);
 		log.info("Executed step[{}#{}] of job[{}#{}]", stepName, stepExecutionId, jobName, jobExecutionId);
 	}
