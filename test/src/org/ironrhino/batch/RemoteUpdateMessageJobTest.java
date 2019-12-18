9b2d215a6b97c09ffb825260cc704367643ed82d
Yanming Zhou <zhouyanming@gmail.com>
2019-12-18 10:57:07 +0800

    Use placeholders

--- test/src/org/ironrhino/batch/RemoteUpdateMessageJobTest.java
+++ test/src/org/ironrhino/batch/RemoteUpdateMessageJobTest.java
@@ -1,8 +1,10 @@
 package org.ironrhino.batch;
 
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @ContextConfiguration(locations = "/resources/batch/remoteUpdateMessage.xml")
+@TestPropertySource(properties = "batch.updateMessage.partitions=50")
 public class RemoteUpdateMessageJobTest extends UpdateMessageJobTestBase {
 
 }
