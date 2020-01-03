c8685e8362cf4f7cf28042dfcc6e37d9379198d9
Yanming Zhou <zhouyanming@gmail.com>
2020-01-03 16:29:51 +0800

    Rename CountAssertionTask to RowsAssertionTask

--- src/org/ironrhino/batch/tasklet/database/RowsAssertionTask.java
+++ src/org/ironrhino/batch/tasklet/database/RowsAssertionTask.java
@@ -1,9 +1,10 @@
-package org.ironrhino.batch.tasklet;
+package org.ironrhino.batch.tasklet.database;
 
 import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.ironrhino.core.jdbc.SqlVerb;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.UnexpectedJobExecutionException;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -14,23 +15,25 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import lombok.Setter;
 
 @Setter
-public class CountAssertionTask implements Tasklet {
+public class RowsAssertionTask implements Tasklet {
 
 	private DataSource dataSource;
 
-	private String countSql;
+	private String sql;
 
 	private Map<String, Object> parameterValues;
 
-	private long expectedResult;
+	private int expectedRows;
 
 	@Override
 	public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-		long actualResult = new NamedParameterJdbcTemplate(dataSource).queryForObject(countSql, parameterValues,
-				long.class);
-		if (actualResult != expectedResult)
+		NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(dataSource);
+		int actualRows = (SqlVerb.parseBySql(sql) == SqlVerb.SELECT)
+				? template.queryForObject(sql, parameterValues, int.class)
+				: template.update(sql, parameterValues);
+		if (actualRows != expectedRows)
 			throw new UnexpectedJobExecutionException(
-					"Expected result is " + expectedResult + " but actual result is " + actualResult);
+					"Expected rows is " + expectedRows + " but actual rows is " + actualRows);
 		return RepeatStatus.FINISHED;
 	}
 
