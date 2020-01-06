8ba0f52923f0bd483d5dca72ed762f1976265bdb
Yanming Zhou <zhouyanming@gmail.com>
2020-01-06 09:57:08 +0800

    Add tasklet tests

---
+++ test/src/org/ironrhino/batch/tasklet/database/RowsAssertionTaskTest.java
@@ -0,0 +1,67 @@
+package org.ironrhino.batch.tasklet.database;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.batch.core.UnexpectedJobExecutionException;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+public class RowsAssertionTaskTest {
+
+	@Test(expected = UnexpectedJobExecutionException.class)
+	public void testUnexpectedJobExecutionException() throws Exception {
+		try (HikariDataSource ds = create(10)) {
+			execute(ds, "select count(*) from test", null, 0);
+		}
+	}
+
+	@Test
+	public void testCount() throws Exception {
+		try (HikariDataSource ds = create(10)) {
+			execute(ds, "select count(*) from test", null, 10);
+			execute(ds, "select count(*) from test where value%2=0", null, 5);
+			execute(ds, "select count(*) from test where value%2=1", null, 5);
+		}
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+		try (HikariDataSource ds = create(10)) {
+			execute(ds, "update test set value=value+1 where value%2=0", null, 5);
+			execute(ds, "select count(*) from test where value%2=1", null, 10);
+		}
+	}
+
+	private static HikariDataSource create(int rows) throws SQLException {
+		HikariDataSource ds = new HikariDataSource();
+		ds.setJdbcUrl("jdbc:h2:mem:test;");
+		try (Connection conn = ds.getConnection(); Statement stmt = conn.createStatement()) {
+			stmt.execute("create table test(value int)");
+			for (int i = 0; i < rows; i++) {
+				stmt.executeUpdate("insert into test values(" + i + ")");
+			}
+		}
+		return ds;
+	}
+
+	private static void execute(DataSource ds, String sql, Map<String, Object> parameterValues, int expectedRows)
+			throws Exception {
+		RowsAssertionTask task = new RowsAssertionTask();
+		task.setDataSource(ds);
+		task.setSql(sql);
+		task.setParameterValues(parameterValues);
+		task.setExpectedRows(expectedRows);
+		assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+	}
+
+}
---
+++ test/src/org/ironrhino/batch/tasklet/file/MoveLastLinesToFirstTaskTest.java
@@ -0,0 +1,70 @@
+package org.ironrhino.batch.tasklet.file;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.core.io.FileSystemResource;
+
+public class MoveLastLinesToFirstTaskTest {
+
+	@Test(expected = IOException.class)
+	public void testIOException() throws Exception {
+		File nonexist = new File(System.getProperty("java.io.tmpdir"), "nonexist.txt");
+		execute(nonexist, -1, 1);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIllegalStateException() throws Exception {
+		execute(createAndWrite(10), 10, 22);
+	}
+
+	@Test
+	public void test() throws Exception {
+		execute(createAndWrite(10), 10, 2);
+	}
+
+	private static File createAndWrite(int lines) throws IOException {
+		File temp = File.createTempFile("test", "txt");
+		try (FileWriter fw = new FileWriter(temp)) {
+			for (int i = 0; i < lines; i++) {
+				fw.write(String.valueOf(i));
+				if (i < lines - 1)
+					fw.write("\n");
+			}
+		}
+		return temp;
+	}
+
+	private static void execute(File file, int totalLines, int movedLines) throws Exception {
+		try {
+			MoveLastLinesToFirstTask task = new MoveLastLinesToFirstTask();
+			task.setFile(new FileSystemResource(file));
+			task.setLines(movedLines);
+			assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+				List<String> list = br.lines().collect(Collectors.toList());
+				assertThat(list.size(), is(totalLines));
+				for (int i = 0; i < movedLines; i++) {
+					assertThat(list.get(i), is(String.valueOf(totalLines - movedLines + i)));
+				}
+				for (int i = movedLines; i < totalLines; i++) {
+					assertThat(list.get(i), is(String.valueOf(i - movedLines)));
+				}
+			}
+		} finally {
+			file.delete();
+		}
+	}
+
+}
