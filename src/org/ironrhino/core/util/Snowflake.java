f6a95dd67682a2aa50056591b9d161a72006793d
Yanming Zhou <zhouyanming@gmail.com>
2020-03-30 12:21:39 +0800

    Allow construct Snowflake with workerIdBits and sequenceBits

--- src/org/ironrhino/core/util/Snowflake.java
+++ src/org/ironrhino/core/util/Snowflake.java
@@ -3,28 +3,35 @@ package org.ironrhino.core.util;
 public class Snowflake {
 
 	private final static long EPOCH = 1556150400000L;
-	private final static long WORKER_ID_BITS = 8L;
-	private final static long MAX_WORKER_ID = -1L ^ -1L << WORKER_ID_BITS;
-	private final static long SEQUENCE_BITS = 10L;
-	private final static long TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
-	private final static long SEQUENCE_MASK = -1L ^ -1L << SEQUENCE_BITS;
 
 	private final int workerId;
+	private final long workerIdBits;
+	private final long sequenceBits;
+	private final long sequenceMask;
+
 	private long sequence = 0L;
 	private long lastTimestamp = -1L;
 
 	public Snowflake(int workerId) {
-		if (workerId > MAX_WORKER_ID || workerId < 0) {
+		this(workerId, 8, 10);
+	}
+
+	public Snowflake(int workerId, int workerIdBits, int sequenceBits) {
+		long maxWorkerId = -1L ^ -1L << workerIdBits;
+		if (workerId > maxWorkerId || workerId < 0) {
 			throw new IllegalArgumentException(
-					String.format("Worker Id can't be greater than %d or less than 0", MAX_WORKER_ID));
+					String.format("workerId can't be greater than %d or less than 0", maxWorkerId));
 		}
 		this.workerId = workerId;
+		this.workerIdBits = workerIdBits;
+		this.sequenceBits = sequenceBits;
+		this.sequenceMask = -1L ^ -1L << sequenceBits;
 	}
 
 	public synchronized long nextId() {
 		long timestamp = System.currentTimeMillis();
 		if (timestamp == lastTimestamp) {
-			sequence = (sequence + 1) & SEQUENCE_MASK;
+			sequence = (sequence + 1) & sequenceMask;
 			if (sequence == 0) {
 				timestamp = System.currentTimeMillis();
 				while (timestamp <= lastTimestamp) {
@@ -38,7 +45,7 @@ public class Snowflake {
 					"Clock moved backwards. Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
 		}
 		lastTimestamp = timestamp;
-		return ((timestamp - EPOCH) << TIMESTAMP_LEFT_SHIFT) | (workerId << SEQUENCE_BITS) | sequence;
+		return ((timestamp - EPOCH) << (sequenceBits + workerIdBits)) | (workerId << sequenceBits) | sequence;
 	}
 
 }
