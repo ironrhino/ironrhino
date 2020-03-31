c4cc8825711df2e7f9c8f7d2e138a0187d592ddd
Yanming Zhou <zhouyanming@gmail.com>
2020-03-31 09:41:57 +0800

    Add default methods to LockService

--- src/org/ironrhino/core/coordination/LockService.java
+++ src/org/ironrhino/core/coordination/LockService.java
@@ -6,9 +6,26 @@ public interface LockService {
 
 	boolean tryLock(String name);
 
-	boolean tryLock(String name, long timeout, TimeUnit unit);
+	default boolean tryLock(String name, long timeout, TimeUnit unit) {
+		boolean success = tryLock(name);
+		long millisTimeout = unit.toMillis(timeout);
+		long start = System.nanoTime();
+		while (!success) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				return false;
+			}
+			if (TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) >= millisTimeout)
+				break;
+			success = tryLock(name);
+		}
+		return success;
+	}
 
-	void lock(String name);
+	default void lock(String name) {
+		tryLock(name, Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+	}
 
 	void unlock(String name);
 
