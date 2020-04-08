d3fb3e3bf32474cdbbee81159880ad80ce04fef8
Yanming Zhou <zhouyanming@gmail.com>
2020-04-08 15:57:05 +0800

    Add file DeleteTask and RenameTask

---
+++ test/src/org/ironrhino/batch/tasklet/file/DeleteTaskTest.java
@@ -0,0 +1,35 @@
+package org.ironrhino.batch.tasklet.file;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.springframework.batch.core.UnexpectedJobExecutionException;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class DeleteTaskTest {
+
+	@Test
+	public void test() throws Exception {
+		File file = File.createTempFile("target", "txt");
+		try {
+			DeleteTask task = new DeleteTask();
+			task.setFile(file);
+			assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+		} finally {
+			file.delete();
+		}
+	}
+
+	@Test(expected = UnexpectedJobExecutionException.class)
+	public void testFileNotExists() throws Exception {
+		File file = File.createTempFile("target", "txt");
+		file.delete();
+		DeleteTask task = new DeleteTask();
+		task.setFile(file);
+		assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+	}
+
+}
---
+++ test/src/org/ironrhino/batch/tasklet/file/RenameTaskTest.java
@@ -0,0 +1,59 @@
+package org.ironrhino.batch.tasklet.file;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.springframework.batch.core.UnexpectedJobExecutionException;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class RenameTaskTest {
+
+	@Test(expected = UnexpectedJobExecutionException.class)
+	public void testTargetExists() throws Exception {
+		File source = File.createTempFile("source", "txt");
+		File target = File.createTempFile("target", "txt");
+		try {
+			RenameTask task = new RenameTask();
+			task.setSource(source);
+			task.setTarget(target);
+			assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+		} finally {
+			source.delete();
+			target.delete();
+		}
+	}
+
+	@Test(expected = UnexpectedJobExecutionException.class)
+	public void testSourceNotExists() throws Exception {
+		File source = File.createTempFile("source", "txt");
+		source.delete();
+		File target = File.createTempFile("target", "txt");
+		try {
+			RenameTask task = new RenameTask();
+			task.setSource(source);
+			task.setTarget(target);
+			assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+		} finally {
+			target.delete();
+		}
+	}
+
+	@Test
+	public void test() throws Exception {
+		File source = File.createTempFile("source", "txt");
+		File target = File.createTempFile("target", "txt");
+		target.delete();
+		try {
+			RenameTask task = new RenameTask();
+			task.setSource(source);
+			task.setTarget(target);
+			assertThat(task.execute(null, null), is(RepeatStatus.FINISHED));
+		} finally {
+			source.delete();
+		}
+	}
+
+}
