7be34d1c0df83c3551e8c2fa17197fe2e5b67b83
Yanming Zhou <zhouyanming@gmail.com>
2021-07-22 16:16:05 +0800

    Use SchedulingConfigurer to do heart beat

--- src/org/ironrhino/core/coordination/impl/RedisMembership.java
+++ src/org/ironrhino/core/coordination/impl/RedisMembership.java
@@ -15,7 +15,6 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.ironrhino.core.coordination.Membership;
@@ -25,20 +24,18 @@ import org.ironrhino.core.util.AppInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
 
 @Component("membership")
 @ServiceImplementationConditional(profiles = { DUAL, CLOUD })
-public class RedisMembership implements Membership {
-
+public class RedisMembership implements Membership, SchedulingConfigurer {
 	private static final String NAMESPACE = "membership:";
 
 	private Set<String> groups = Collections.newSetFromMap(new ConcurrentHashMap<>());
-
-	@Autowired
-	private TaskScheduler taskScheduler;
 
 	@Value("${redis.membership.heartbeat:60000}")
 	private int heartbeat = 60000;
@@ -48,63 +45,16 @@ public class RedisMembership implements Membership {
 	@PriorityQualifier
 	private StringRedisTemplate coordinationStringRedisTemplate;
 
-	@PostConstruct
-	public void afterPropertiesSet() {
-		if (AppInfo.getContextPath() == null) // not in servlet container
-			return;
-		taskScheduler.scheduleAtFixedRate(() -> {
-			for (String group : groups) {
-				List<String> members = getMembers(group);
-				String self = AppInfo.getInstanceId();
-				if (!members.contains(self))
-					coordinationStringRedisTemplate.opsForList().rightPush(NAMESPACE + group, self);
-				for (String member : members) {
-					if (member.equals(self))
-						continue;
-					boolean alive = false;
-					String url = "http://" + member.substring(member.lastIndexOf('@') + 1)
-							+ "/_ping?_internal_testing_";
-					try {
-						HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-						conn.setConnectTimeout(3000);
-						conn.setReadTimeout(2000);
-						conn.setInstanceFollowRedirects(false);
-						conn.setDoOutput(false);
-						conn.setUseCaches(false);
-						conn.connect();
-						if (conn.getResponseCode() == 200) {
-							try (BufferedReader br = new BufferedReader(
-									new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
-								String value = br.lines().collect(Collectors.joining("\n"));
-								if (value.equals(member)) {
-									alive = true;
-								} else {
-									if (!members.contains(value) && value.length() <= 100
-											&& value.matches("[\\w-]+@[\\w.:]+")) {
-										if (AppInfo.getAppName().equals(value.substring(0, value.lastIndexOf('-')))) {
-											coordinationStringRedisTemplate.opsForList().rightPush(NAMESPACE + group,
-													value);
-										} else {
-											// multiple virtual host
-											alive = true;
-										}
-									}
-								}
-							}
-						}
-						conn.disconnect();
-					} catch (IOException e) {
-					}
-					if (!alive)
-						coordinationStringRedisTemplate.opsForList().remove(NAMESPACE + group, 0, member);
-				}
-			}
-		}, heartbeat);
-	}
-
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public void join(String group) {
-		coordinationStringRedisTemplate.opsForList().leftPush(NAMESPACE + group, AppInfo.getInstanceId());
+		String key = NAMESPACE + group;
+		String self = AppInfo.getInstanceId();
+		coordinationStringRedisTemplate.executePipelined((SessionCallback) redisOperations -> {
+			redisOperations.opsForList().remove(key, 1, self);
+			redisOperations.opsForList().leftPush(key, self);
+			return null;
+		});
 		groups.add(group);
 	}
 
@@ -136,6 +86,62 @@ public class RedisMembership implements Membership {
 	public void destroy() {
 		for (String group : groups)
 			coordinationStringRedisTemplate.opsForList().remove(NAMESPACE + group, 0, AppInfo.getInstanceId());
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		if (AppInfo.getContextPath() != null) // in container
+			taskRegistrar.addFixedDelayTask(this::doHeartbeat, heartbeat);
+	}
+
+	private void doHeartbeat() {
+
+		for (String group : groups) {
+			List<String> members = getMembers(group);
+			String self = AppInfo.getInstanceId();
+			if (!members.contains(self))
+				coordinationStringRedisTemplate.opsForList().rightPush(NAMESPACE + group, self);
+			for (String member : members) {
+				if (member.equals(self))
+					continue;
+				boolean alive = false;
+				String url = "http://" + member.substring(member.lastIndexOf('@') + 1) + "/_ping?_internal_testing_";
+				try {
+					HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+					conn.setConnectTimeout(3000);
+					conn.setReadTimeout(2000);
+					conn.setInstanceFollowRedirects(false);
+					conn.setDoOutput(false);
+					conn.setUseCaches(false);
+					conn.connect();
+					if (conn.getResponseCode() == 200) {
+						try (BufferedReader br = new BufferedReader(
+								new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+							String value = br.lines().collect(Collectors.joining("\n"));
+							if (value.equals(member)) {
+								alive = true;
+							} else {
+								if (!members.contains(value) && value.length() <= 100
+										&& value.matches("[\\w-]+@[\\w.:]+")) {
+									if (AppInfo.getAppName().equals(value.substring(0, value.lastIndexOf('-')))) {
+										coordinationStringRedisTemplate.opsForList().rightPush(NAMESPACE + group,
+												value);
+									} else {
+										// multiple virtual host
+										alive = true;
+									}
+								}
+							}
+						}
+					}
+					conn.disconnect();
+				} catch (IOException e) {
+				}
+				if (!alive)
+					coordinationStringRedisTemplate.opsForList().remove(NAMESPACE + group, 0, member);
+			}
+		}
+
 	}
 
 }
