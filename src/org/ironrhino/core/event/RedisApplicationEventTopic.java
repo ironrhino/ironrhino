d934ce43ace89fc5dba9931d491bac235b17b656
Yanming Zhou <zhouyanming@gmail.com>
2021-09-08 09:04:57 +0800

    Break circular dependency

--- src/org/ironrhino/core/event/RedisApplicationEventTopic.java
+++ src/org/ironrhino/core/event/RedisApplicationEventTopic.java
@@ -4,9 +4,9 @@ import static org.ironrhino.core.metadata.Profiles.CLOUD;
 import static org.ironrhino.core.metadata.Profiles.CLUSTER;
 import static org.ironrhino.core.metadata.Profiles.DUAL;
 
-import org.ironrhino.core.metadata.Scope;
 import org.ironrhino.core.redis.RedisTopic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -16,11 +16,11 @@ import org.springframework.stereotype.Component;
 public class RedisApplicationEventTopic extends RedisTopic<ApplicationEvent> implements ApplicationEventTopic {
 
 	@Autowired
-	private EventPublisher eventPublisher;
+	private ApplicationContext ctx;
 
 	@Override
 	public void subscribe(ApplicationEvent event) {
-		eventPublisher.publish(event, Scope.LOCAL);
+		ctx.publishEvent(event);
 	}
 
 }
