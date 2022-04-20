25fa662f1804f2ab6f1dbbc192de3bc43375c886
Yanming Zhou <zhouyanming@gmail.com>
2022-04-20 10:15:48 +0800

    Avoid file upload DOS

--- src/org/ironrhino/core/struts/interceptor/MyParametersInterceptor.java
+++ src/org/ironrhino/core/struts/interceptor/MyParametersInterceptor.java
@@ -1,8 +1,10 @@
 package org.ironrhino.core.struts.interceptor;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.ironrhino.core.util.ReflectionUtils;
 
@@ -35,8 +37,20 @@ public class MyParametersInterceptor extends ParametersInterceptor {
 
 	@Override
 	protected void setParameters(Object action, ValueStack stack, Map<String, Object> parameters) {
-		action = ReflectionUtils.getTargetObject(action);
+		cleanup(parameters); // S2-60
+		action = ReflectionUtils.getTargetObject(action); // S2-49
 		super.setParameters(action, stack, parameters);
+	}
+
+	private void cleanup(Map<String, Object> parameters) {
+		List<String> fileParams = parameters.entrySet().stream().filter(entry -> entry.getValue() instanceof File[])
+				.map(Map.Entry::getKey).collect(Collectors.toList());
+		for (String fileParam : fileParams) {
+			List<String> tobeRemoved = parameters.keySet().stream()
+					.filter(key -> key.startsWith(fileParam + '.') || key.startsWith(fileParam + '['))
+					.collect(Collectors.toList());
+			tobeRemoved.forEach(parameters::remove);
+		}
 	}
 
 }
